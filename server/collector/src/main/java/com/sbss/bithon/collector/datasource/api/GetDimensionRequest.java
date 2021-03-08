3deb7fc90fa0fa07c85b969ad1fb1f0c7bb56225
frank chen <frank.chen021@outlook.com>
2021-03-08 14:47:06 +0800

    add JVM GC schema
    Signed-off-by: frank chen <frank.chen021@outlook.com>

---
+++ server/collector/src/main/java/com/sbss/bithon/collector/datasource/api/GetDimensionRequest.java
@@ -0,0 +1,31 @@
+package com.sbss.bithon.collector.datasource.api;
+
+import com.sbss.bithon.collector.datasource.storage.DimensionCondition;
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * @author frank.chen021@outlook.com
+ * @date 2021/3/7 5:29 下午
+ */
+@Data
+public class GetDimensionRequest {
+
+    @NotEmpty
+    private String startTimeISO8601;
+
+    @NotEmpty
+    private String endTimeISO8601;
+
+    @NotNull
+    private String dataSource;
+
+    private Collection<DimensionCondition> conditions = Collections.emptyList();
+
+    @NotNull
+    private String dimension;
+}
