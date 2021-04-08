8d5d4019d054af9539fc628eb833241fbd31c687
frank chen <frank.chen021@outlook.com>
2021-04-08 21:36:58 +0800

    Replace log4j by log4j2 to eliminate CVE-2019-17571
    Signed-off-by: frank chen <frank.chen021@outlook.com>

--- agent/agent-core/src/main/java/com/sbss/bithon/agent/core/bootstrap/AgentStarter.java
+++ agent/agent-core/src/main/java/com/sbss/bithon/agent/core/bootstrap/AgentStarter.java
@@ -4,9 +4,12 @@ import com.sbss.bithon.agent.core.context.AgentContext;
 import com.sbss.bithon.agent.core.plugin.loader.PluginDependencyManager;
 import com.sbss.bithon.agent.core.plugin.loader.PluginInstaller;
 import com.sbss.bithon.agent.core.setting.AgentSettingManager;
-import shaded.org.apache.log4j.xml.DOMConfigurator;
+import shaded.org.apache.logging.log4j.core.config.ConfigurationSource;
+import shaded.org.apache.logging.log4j.core.config.Configurator;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.lang.instrument.Instrumentation;
 
 import static java.io.File.separator;
@@ -32,7 +35,9 @@ public class AgentStarter {
         PluginInstaller.install(agentContext, inst);
     }
 
-    private void initAgentLogger(String agentPath) {
+    private void initAgentLogger(String agentPath) throws IOException {
+
+
         String logConfigName = "log4j.configuration";
         String logConfigFile = agentPath + separator + AgentContext.CONF_DIR + separator + "log4j.xml";
         String oldLogConfig = System.getProperty(logConfigName);
@@ -41,10 +46,12 @@ public class AgentStarter {
         System.setProperty(logConfigName, logConfigFile);
 
         // log config
-        DOMConfigurator.configure(logConfigFile);
+        Configurator.initialize(null,
+                                new ConfigurationSource(new FileInputStream(logConfigFile),
+                                                        new File(logConfigFile).toURI().toURL()));
 
         // restore original property
-        if ( oldLogConfig != null ) {
+        if (oldLogConfig != null) {
             System.setProperty(logConfigName, oldLogConfig);
         } else {
             System.clearProperty(logConfigName);
