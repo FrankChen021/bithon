e98d7266541b0a7a0ed12d4ca6342a6c1b78da7e
frank chen <frank.chen021@outlook.com>
2021-04-16 23:33:34 +0800

    Improve style (#62)
    Signed-off-by: frank chen <frank.chen021@outlook.com>

--- server/web-app/src/main/resources/static/js/chart-component.js
+++ server/web-app/src/main/resources/static/js/chart-component.js
@@ -7,7 +7,7 @@ class ChartComponent {
 
         this._chartId = option.containerId + '_chart';
         this._card = $('#' + option.containerId).append(
-            '    <div class="card card-block">                             ' +
+            '    <div class="card card-block" style="border-radius: .0em !important;">                             ' +
             '        <div class="card-body" style="padding: 0.25em">                    ' +
             '            <div class="card-chart"></div> ' +
             '        </div>                                     ' +
--- server/web-app/src/main/resources/static/js/component/time-interval/component.js
+++ server/web-app/src/main/resources/static/js/component/time-interval/component.js
@@ -4,28 +4,40 @@ class TimeInterval {
         this._listeners = [];
 
         this._control = $('<select id="intervalSelector" class="form-control">       ' +
-            '    <option value="1" data-unit="minute">1 min</option>   ' +
-            '    <option value="3" data-unit="minute">3 min</option>   ' +
-            '    <option value="5" data-unit="minute" selected>5 min</option>   ' +
-            '    <option value="15" data-unit="minute">15 min</option> ' +
-            '    <option value="30" data-unit="minute">30 min</option> ' +
-            '    <option value="1" data-unit="hour">1 hour</option>    ' +
-            '    <option value="3" data-unit="hour">3 hour</option>    ' +
-            '    <option value="6" data-unit="hour">6 hour</option>    ' +
-            '    <option value="12" data-unit="hour">12 hour</option>  ' +
-            '    <option value="24" data-unit="hour">24 hour</option>  ' +
+            '    <option value="1" data-unit="minute">Last 1m</option>   ' +
+            '    <option value="3" data-unit="minute">Last 3m</option>   ' +
+            '    <option value="5" data-unit="minute" selected>Last 5m</option>   ' +
+            '    <option value="15" data-unit="minute">Last 15m</option> ' +
+            '    <option value="30" data-unit="minute">Last 30m</option> ' +
+            '    <option value="1" data-unit="hour">Last 1h</option>    ' +
+            '    <option value="3" data-unit="hour">Last 3h</option>    ' +
+            '    <option value="6" data-unit="hour">Last 6h</option>    ' +
+            '    <option value="12" data-unit="hour">Last 12h</option>  ' +
+            '    <option value="24" data-unit="hour">Last 24h</option>  ' +
+            '    <option value="today" data-unit="hour">Today</option>  ' +
             '</select>                                                 ');
 
         $(this._control).change(() => {
             const option = $(this._control).find("option:selected");
             const val = option.val();
-            const unit = option.attr('data-unit');
-            const fn = function () {
-                return {
-                    start: moment().utc().subtract(val, unit).local().toISOString(),
-                    end: moment().utc().local().toISOString()
-                }
-            };
+
+            let fn;
+            if ( val === 'today') {
+                fn = function () {
+                    return {
+                        start: moment().utc().local().startOf('day').toISOString(),
+                        end: moment().utc().local().toISOString()
+                    }
+                };
+            } else {
+                const unit = option.attr('data-unit');
+                fn = function () {
+                    return {
+                        start: moment().utc().subtract(val, unit).local().toISOString(),
+                        end: moment().utc().local().toISOString()
+                    }
+                };
+            }
             $.each(this._listeners, (index, listener) => {
                 listener(fn);
             });
