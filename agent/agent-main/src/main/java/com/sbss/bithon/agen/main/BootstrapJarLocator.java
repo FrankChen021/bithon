17d8f4e31b646eafd3214faaa107df9f1bc1a890
frank chen <frank.chen021@outlook.com>
2021-04-27 23:56:48 +0800

    agent-boostrap ---> agent-main

--- agent/agent-main/src/main/java/com/sbss/bithon/agen/main/BootstrapJarLocator.java
+++ agent/agent-main/src/main/java/com/sbss/bithon/agen/main/BootstrapJarLocator.java
@@ -14,7 +14,7 @@
  *    limitations under the License.
  */
 
-package com.sbss.bithon.agent.bootstrap;
+package com.sbss.bithon.agen.main;
 
 import com.sbss.bithon.agent.boot.expt.AgentException;
 
