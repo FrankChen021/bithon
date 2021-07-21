530f19b43d6507b3dfb38802b6f21506ff6b3f18
frank chen <frank.chen021@outlook.com>
2021-07-21 18:21:32 +0800

    Remove slf4j initialization from agent

--- agent/agent-plugins/logging-logback/src/main/java/com/sbss/bithon/agent/plugin/logback/interceptor/LoggerCallAppenders.java
+++ agent/agent-plugins/logging-logback/src/main/java/com/sbss/bithon/agent/plugin/logback/interceptor/LoggerCallAppenders.java
@@ -31,24 +31,26 @@ public class LoggerCallAppenders extends AbstractInterceptor {
     private LogMetricCollector counter;
 
     @Override
-    public boolean initialize() {
-        counter = MetricCollectorManager.getInstance().register("logback", new LogMetricCollector());
-        return true;
-    }
-
-    @Override
     public void onMethodLeave(AopContext context) {
         ILoggingEvent iLoggingEvent = (ILoggingEvent) context.getArgs()[0];
         if (iLoggingEvent.getLevel().toInt() != Level.ERROR.toInt()) {
             return;
         }
         IThrowableProxy exception = iLoggingEvent.getThrowableProxy();
-        if (null != exception) {
-            counter.addException((String) InterceptorContext.get("uri"),
-                                 exception);
-            //this.addTrace(exception.getMessage());
-        } else if (iLoggingEvent.getMessage() != null) {
-            //this.addTrace(iLoggingEvent.getMessage());
+        if (null == exception) {
+            return;
         }
+
+        if (counter == null) {
+            synchronized (this) {
+                //double check
+                if (counter == null) {
+                    counter = MetricCollectorManager.getInstance().register("logback", new LogMetricCollector());
+                }
+            }
+        }
+
+        counter.addException((String) InterceptorContext.get("uri"),
+                             exception);
     }
 }
