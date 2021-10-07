61c57f57ca458c66e9b30202fa9778ee78c02d48
Frank Chen <frank.chen021@outlook.com>
2021-10-07 20:21:07 +0800

    Add WebFlux Support (#161)
    * Add WebFlux Support
    
    * Support to record exceptions in WebFlux
    
    * Fix count4xx/count5xx metrics names in BRPC message
    
    * Intercept at the entry of webflux server to get metrics correctly
    
    * Add HTTP body size collector

---
+++ agent/agent-plugins/spring-webflux/src/main/java/com/sbss/bithon/agent/plugin/spring/webflux/SpringWebFluxPlugin.java
@@ -0,0 +1,53 @@
+/*
+ *    Copyright 2020 bithon.cn
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.sbss.bithon.agent.plugin.spring.webflux;
+
+import com.sbss.bithon.agent.core.aop.descriptor.InterceptorDescriptor;
+import com.sbss.bithon.agent.core.aop.descriptor.MethodPointCutDescriptorBuilder;
+import com.sbss.bithon.agent.core.plugin.IPlugin;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static com.sbss.bithon.agent.core.aop.descriptor.InterceptorDescriptorBuilder.forClass;
+
+/**
+ * @author frankchen
+ * @date 2021-09-22 23:36
+ */
+public class SpringWebFluxPlugin implements IPlugin {
+
+    @Override
+    public List<InterceptorDescriptor> getInterceptors() {
+        return Arrays.asList(
+            forClass("org.springframework.http.server.reactive.ReactorHttpHandlerAdapter")
+                .methods(
+                    MethodPointCutDescriptorBuilder.build()
+                                                   .onAllMethods("apply")
+                                                   .to("com.sbss.bithon.agent.plugin.spring.webflux.interceptor.ReactorHttpHandlerAdapter$Apply")
+                ),
+
+            forClass("reactor.netty.http.server.HttpServerConfig$HttpServerChannelInitializer")
+                .methods(
+                    MethodPointCutDescriptorBuilder.build()
+                                                   .debug()
+                                                   .onAllMethods("onChannelInit")
+                                                   .to("com.sbss.bithon.agent.plugin.spring.webflux.interceptor.HttpServerChannelInitializer$OnChannelInit")
+                )
+        );
+    }
+}
