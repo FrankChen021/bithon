FrankChen021/bithon

commit 8258fcdd2b9fe53861873ac7a5911232e7ef4653

Update dashboard names api (#190)
* Use DashboardApi instead of DataSourceApi to get dashboard list

* Update Exception

server/web-app/src/main/java/com/sbss/bithon/server/webapp/ui/DashboardController.java
@@ -16,47 +16,160 @@
 
 package com.sbss.bithon.server.webapp.ui;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import sun.misc.Launcher;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Comparator;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.stream.Collectors;
 
 /**
+ * This is a temporary solution that reads configuration from file
+ * in future it should read from a remote storage such as DB
+ *
  * @author frank.chen021@outlook.com
  * @date 2021-10-01
  */
 @RestController
 public class DashboardController {
 
-    @GetMapping("/web/api/dashboard/get/{boardName}")
-    public void getBoardConfig(@PathVariable("boardName") String boardName, HttpServletResponse response) {
-        response.setContentType("application/json");
+    @Getter
+    @AllArgsConstructor
+    public class DisplayableText {
+        private final String value;
+        private final String text;
+    }
 
-        // for now, it loads config from static file
-        // in future it can be changed to load from external storage
-        try (InputStream is = DashboardController.class.getClassLoader().getResourceAsStream("dashboard/" + boardName + ".json")) {
-            if (is == null) {
-                response.setStatus(HttpStatus.NOT_FOUND.value());
-                return;
+    private Map<String, LoadedDashboard> dashboardConfigs = new HashMap<>();
+    private List<DisplayableText> dashboardNames;
+    private final ObjectMapper om;
+
+    public void loadDashboard() throws IOException {
+        final String path = "dashboard";
+        final File jarFile = new File(getClass().getProtectionDomain().getCodeSource().getLocation().getPath());
+
+        if (jarFile.isFile()) {
+            final JarFile jar = new JarFile(jarFile);
+            final Enumeration<JarEntry> entries = jar.entries();
+            while (entries.hasMoreElements()) {
+                JarEntry jarEntry = entries.nextElement();
+                final String name = jarEntry.getName();
+                if (name.startsWith(path + "/") && name.endsWith(".json")) {
+                    LoadedDashboard board = LoadedDashboard.load(om,
+                                                                 name,
+                                                                 jar.getInputStream(jarEntry));
+                    dashboardConfigs.put(board.id, board);
+                }
             }
+            jar.close();
+        } else { // Run with IDE
+            final URL url = Launcher.class.getResource("/" + path);
+            if (url != null) {
+                try {
+                    final File dir = new File(url.toURI());
+                    for (File file : dir.listFiles()) {
+                        if (!file.getName().endsWith(".json")) {
+                            continue;
+                        }
 
-            byte[] buf = new byte[1024];
-            int len = 0;
-            do {
-                len = is.read(buf);
-                response.getOutputStream().write(buf, 0, len);
-            } while (len == 1024);
-            response.setStatus(HttpStatus.OK.value());
-        } catch (Exception e) {
-            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
-            try {
-                response.getWriter().write(e.toString());
-            } catch (IOException ignored) {
+                        try (InputStream is = new FileInputStream(file)) {
+                            LoadedDashboard board = LoadedDashboard.load(om,
+                                                                         file.getName(),
+                                                                         is);
+                            dashboardConfigs.put(board.id, board);
+                        }
+                    }
+                } catch (URISyntaxException ex) {
+                    // never happens
+                }
             }
         }
+    }
+
+    @Data
+    static class Dashboard {
+        private String title;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class LoadedDashboard {
+        private String id;
+        private String title;
+        private byte[] stream;
+
+        public static LoadedDashboard load(ObjectMapper om, String name, InputStream is) throws IOException {
+            byte[] stream = toByteArray(is);
+            Dashboard board = om.readValue(stream, Dashboard.class);
+
+            return new LoadedDashboard(name.substring(0, name.length() - ".json".length()),
+                                       board.getTitle(),
+                                       stream);
+        }
+
+        private static byte[] toByteArray(InputStream is) throws IOException {
+            try (ByteArrayOutputStream bs = new ByteArrayOutputStream()) {
+                byte[] buf = new byte[1024];
+                int len;
+                do {
+                    len = is.read(buf);
+                    bs.write(buf, 0, len);
+                } while (len == 1024);
+                return bs.toByteArray();
+            }
+        }
+
+    }
+
+    public DashboardController(ObjectMapper om) {
+        this.om = om;
+        try {
+            loadDashboard();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.dashboardNames = this.dashboardConfigs.values()
+                                                   .stream()
+                                                   .map(dashboard -> new DisplayableText(dashboard.id, dashboard.title))
+                                                   .sorted(Comparator.comparing(o -> o.text))
+                                                   .collect(Collectors.toList());
+    }
+
+    @GetMapping("/web/api/dashboard/names")
+    public List<DisplayableText> getDashBoardConfig() {
+        return dashboardNames;
+    }
+
+    @GetMapping("/web/api/dashboard/get/{boardName}")
+    public void getDashBoardConfig(@PathVariable("boardName") String boardName, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+
+        LoadedDashboard board = dashboardConfigs.get(boardName);
+        if (board == null) {
+            response.setStatus(HttpStatus.NOT_FOUND.value());
+            return;
+        }
+        response.getOutputStream().write(board.getStream());
+        response.setStatus(HttpStatus.OK.value());
     }
 }

server/collector/src/main/java/com/sbss/bithon/server/metric/DataSourceNotFoundException.java
@@ -1,0 +1,27 @@
+/*
+ *    Copyright 2020 bithon.cn
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.sbss.bithon.server.metric;
+
+/**
+ * @author Frank Chen
+ * @date 7/10/21 11:49 am
+ */
+public class DataSourceNotFoundException extends RuntimeException {
+    public DataSourceNotFoundException(String name) {
+        super("Can't find schema for datasource " + name);
+    }
+}

server/collector/src/main/java/com/sbss/bithon/server/metric/DataSourceSchemaManager.java
@@ -83,7 +83,7 @@ public class DataSourceSchemaManager implements SmartLifecycle {
                 throw new RuntimeException(e);
             }
         }
-        throw new RuntimeException("Can't find schema for datasource " + name);
+        throw new DataSourceNotFoundException("Can't find schema for datasource " + name);
     }
 
     public Map<String, DataSourceSchema> getDataSources() {

server/web-app/src/main/resources/static/js/api-client/dashboard-api.js
@@ -16,4 +16,22 @@ class DashboardApi {
             }
         });
     }
+
+    getDashboardList(successCallback, errorCallback) {
+        $.ajax({
+            type: 'GET',
+            url: "/web/api/dashboard/names",
+            async: true,
+            dataType: "json",
+            contentType: "application/json",
+            success: (data) => {
+                successCallback(data);
+            },
+            error: (data) => {
+                errorCallback(data);
+            }
+        });
+    }
 }
+
+const g_DashboardApi = new DashboardApi();

server/web-app/src/main/resources/static/js/component/sidebar/component.js
@@ -1,8 +1,7 @@
 class MetricSidebar {
-    constructor(containerId, appName, schemaApi) {
+    constructor(containerId, appName) {
         this._container = $('#' + containerId);
         this._appName = appName;
-        this._schemaApi = schemaApi;
     }
 
     load() {
@@ -37,7 +36,7 @@ class MetricSidebar {
         // $.each(data, (index, item) => {
         //     this.addMetricItem(item);
         // });
-        this._schemaApi.getNames(
+        g_DashboardApi.getDashboardList(
             (data) => {
                 $.each(data, (index, item) => {
                     this.addMetricItem({id: item.value, text: item.text});

server/web-app/src/main/resources/templates/app/metric-template.html
@@ -2,30 +2,31 @@
 <html lang="zh_CN"
       xmlns:th="http://www.thymeleaf.org"
       xmlns:layout="http://www.ultraq.net.nz/thymeleaf/layout"
-      layout:decorate="~{app/app-layout}">
+      layout:decorate="~{app/app-layout}"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.ultraq.net.nz/thymeleaf/layout ">
 <head>
     <title>Bithon</title>
-    <script th:src="@{'/js/api-client/dashboard-api.js'}"></script>
 </head>
 <body>
 
 <div layout:fragment="content" id="dashboard">
 
-<script th:inline="javascript">
-$(document).ready(function () {
-    new DashboardApi().getDashboardConfig([[${metricName}]],
-        (boardConfig) => {
-            const dashboard = new Dashboard('dashboard', [[${appName}]], new SchemaApi());
-            dashboard.load(boardConfig);
+    <script th:inline="javascript">
+        $(document).ready(function () {
+            new DashboardApi().getDashboardConfig([[${metricName}]],
+                (boardConfig) => {
+                    const dashboard = new Dashboard('dashboard', [[${appName}]], new SchemaApi());
+                    dashboard.load(boardConfig);
 
-            $(window).bind('resize', () => {
-                dashboard.resize();
-            });
-        }, (error) => {
-            alert(error.responseText);
+                    $(window).bind('resize', () => {
+                        dashboard.resize();
+                    });
+                }, (error) => {
+                    alert(error.responseText);
+                });
         });
-});
-</script>
+    </script>
 
 </div>
 </body>

commit 8ac1a993da4be265f006c60ad52b0de7a7fd2ac5

Fix bug introduced by pr #190

server/web-app/src/main/java/com/sbss/bithon/server/webapp/ui/DashboardController.java
@@ -20,27 +20,23 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.support.ResourcePatternUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import sun.misc.Launcher;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.Comparator;
-import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
 import java.util.stream.Collectors;
 
 /**
@@ -50,59 +46,48 @@ import java.util.stream.Collectors;
  * @author frank.chen021@outlook.com
  * @date 2021-10-01
  */
+@Slf4j
 @RestController
 public class DashboardController {
 
     @Getter
     @AllArgsConstructor
-    public class DisplayableText {
+    public static class DisplayableText {
         private final String value;
         private final String text;
     }
 
-    private Map<String, LoadedDashboard> dashboardConfigs = new HashMap<>();
-    private List<DisplayableText> dashboardNames;
-    private final ObjectMapper om;
+    private final Map<String, LoadedDashboard> dashboardConfigs;
+    private final List<DisplayableText> dashboardNames;
 
-    public void loadDashboard() throws IOException {
-        final String path = "dashboard";
-        final File jarFile = new File(getClass().getProtectionDomain().getCodeSource().getLocation().getPath());
+    static class DashboardLoader {
+        private final ObjectMapper om;
+        private final ResourceLoader resourceLoader;
 
-        if (jarFile.isFile()) {
-            final JarFile jar = new JarFile(jarFile);
-            final Enumeration<JarEntry> entries = jar.entries();
-            while (entries.hasMoreElements()) {
-                JarEntry jarEntry = entries.nextElement();
-                final String name = jarEntry.getName();
-                if (name.startsWith(path + "/") && name.endsWith(".json")) {
-                    LoadedDashboard board = LoadedDashboard.load(om,
-                                                                 name,
-                                                                 jar.getInputStream(jarEntry));
-                    dashboardConfigs.put(board.id, board);
-                }
-            }
-            jar.close();
-        } else { // Run with IDE
-            final URL url = Launcher.class.getResource("/" + path);
-            if (url != null) {
-                try {
-                    final File dir = new File(url.toURI());
-                    for (File file : dir.listFiles()) {
-                        if (!file.getName().endsWith(".json")) {
-                            continue;
-                        }
+        DashboardLoader(ObjectMapper om, ResourceLoader resourceLoader) {
+            this.om = om;
+            this.resourceLoader = resourceLoader;
+        }
 
-                        try (InputStream is = new FileInputStream(file)) {
-                            LoadedDashboard board = LoadedDashboard.load(om,
-                                                                         file.getName(),
-                                                                         is);
-                            dashboardConfigs.put(board.id, board);
-                        }
+        public Map<String, LoadedDashboard> loadDashboard() {
+            Map<String, LoadedDashboard> dashboardConfigs = new HashMap<>();
+            try {
+                Resource[] resources = ResourcePatternUtils.getResourcePatternResolver(resourceLoader)
+                                                           .getResources("classpath:/dashboard/*.json");
+                for (Resource resource : resources) {
+                    try {
+                        LoadedDashboard board = LoadedDashboard.load(om,
+                                                                     resource.getFilename(),
+                                                                     resource.getInputStream());
+                        dashboardConfigs.put(board.id, board);
+                    } catch (IOException e) {
+                        log.error("Error loading {}: {}}", resource.getFilename(), e.getMessage());
                     }
-                } catch (URISyntaxException ex) {
-                    // never happens
                 }
+            } catch (IOException e) {
+                log.error("Error when loading dashboard resources", e);
             }
+            return dashboardConfigs;
         }
     }
 
@@ -138,16 +123,10 @@ public class DashboardController {
                 return bs.toByteArray();
             }
         }
-
     }
 
-    public DashboardController(ObjectMapper om) {
-        this.om = om;
-        try {
-            loadDashboard();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public DashboardController(ObjectMapper om, ResourceLoader resourceLoader) {
+        this.dashboardConfigs = new DashboardLoader(om, resourceLoader).loadDashboard();
         this.dashboardNames = this.dashboardConfigs.values()
                                                    .stream()
                                                    .map(dashboard -> new DisplayableText(dashboard.id, dashboard.title))