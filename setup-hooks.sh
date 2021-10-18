96a19f15b0b121b106c65d24b66daae23d8ba517
frank chen <frank.chen021@outlook.com>
2021-10-18 10:04:40 +0800

    Fix path

--- setup-hooks.sh
+++ setup-hooks.sh
@@ -14,4 +14,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ln -s hooks/pre-push.sh .git/hooks/pre-push
+ln -s ../../hooks/pre-push.sh .git/hooks/pre-push
