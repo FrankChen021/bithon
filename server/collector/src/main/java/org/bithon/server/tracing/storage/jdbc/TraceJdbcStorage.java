36da21f3528032569ee5a5834bc1bcdaa5735e17
frank chen <frank.chen021@outlook.com>
2021-10-22 23:09:47 +0800

    Fix import

--- server/collector/src/main/java/org/bithon/server/tracing/storage/jdbc/TraceJdbcStorage.java
+++ server/collector/src/main/java/org/bithon/server/tracing/storage/jdbc/TraceJdbcStorage.java
@@ -19,6 +19,7 @@ package org.bithon.server.tracing.storage.jdbc;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.bithon.component.db.jooq.Indexes;
 import org.bithon.component.db.jooq.Tables;
 import org.bithon.component.db.jooq.tables.records.BithonTraceSpanRecord;
