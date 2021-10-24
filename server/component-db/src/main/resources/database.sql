1ebcf82b54ad23971c0b30126905ad07e957b791
frank chen <frank.chen021@outlook.com>
2021-10-24 23:36:42 +0800

    Add ClickHouse as storage

--- server/component-db/src/main/resources/database.sql
+++ server/component-db/src/main/resources/database.sql
@@ -32,18 +32,23 @@ CREATE TABLE `bithon_application` (
   KEY `idx_group`(`group`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='应用';
 
-DROP TABLE IF EXISTS `bithon_metadata`;
-
-CREATE TABLE `bithon_metadata` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '唯一编号',
-  `name` varchar(128) NOT NULL DEFAULT '' COMMENT '名称',
-  `type` varchar(64) NOT NULL DEFAULT '' COMMENT '环境',
-  `parent_id` bigint(20) NOT NULL DEFAULT '0' COMMENT '父',
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `uq_name` (`name`,`type`, `parent_id`)
+DROP TABLE IF EXISTS `bithon_application_instance`;
+# RDMS such as MySQL/H2
+CREATE TABLE `bithon_application_instance` (
+  `timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'update time',
+  `application_name` varchar(128) NOT NULL,
+  `application_type` varchar(64) NOT NULL,
+  `instance_name` varchar(64) NOT NULL,
+  UNIQUE KEY `uq_name` (`application_name`,`application_type`, `instance_name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8mb4 COMMENT='应用';
+
+# ClickHouse
+CREATE TABLE `bithon_application_instance` (
+   `timestamp` datetime NOT NULL,
+   `application_name` varchar(128) NOT NULL,
+   `application_type` varchar(64) NOT NULL,
+   `instance_name` varchar(64) NOT NULL
+) ENGINE=MergeTree ORDER BY (`application_name`, `application_type`, `instance_name`);
 
 DROP TABLE IF EXISTS `bithon_agent_setting`;
 CREATE TABLE `bithon_agent_setting` (
@@ -59,7 +64,6 @@ CREATE TABLE `bithon_agent_setting` (
 
 DROP TABLE IF EXISTS `bithon_trace_span`;
 CREATE TABLE `bithon_trace_span` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
   `app_name` VARCHAR(64) NOT NULL COMMENT '',
   `instance_name` VARCHAR(64) NOT NULL COMMENT '',
   `name` VARCHAR(64) NOT NULL COMMENT '',
@@ -72,12 +76,25 @@ CREATE TABLE `bithon_trace_span` (
   `costTime` BIGINT NOT NULL DEFAULT 0 COMMENT '',
   `tags` TEXT COMMENT '',
   `timestamp` TIMESTAMP NOT NULL COMMENT 'Milli Seconds',
-  PRIMARY KEY `idx_id` (`id`),
   KEY `idx_app_name` (`app_name`),
   KEY `idx_instanceName` (`instance_name`),
   UNIQUE `idx_key` (`traceId`, `spanId`),
   KEY `idx_parentSpanId` (`parentSpanId`)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+# ClickHouse
+CREATE TABLE `bithon_trace_span` (`app_name` VARCHAR(64) NOT NULL ,
+                                     `instance_name` VARCHAR(64) NOT NULL ,
+                                     `name` VARCHAR(64) NOT NULL ,
+                                     `clazz` varchar(128) NOT NULL ,
+                                     `method` VARCHAR(128) NOT NULL ,
+                                     `traceId` VARCHAR(64) DEFAULT '' NOT NULL ,
+                                     `spanId` VARCHAR(64) DEFAULT '' NOT NULL ,
+                                     `parentSpanId` VARCHAR(64)  NOT NULL ,
+                                     `kind` VARCHAR(64) NOT NULL ,
+                                     `costTime` BIGINT NOT NULL , `tags` TEXT , `timestamp` TIMESTAMP(3) NOT NULL ) Engine=MergeTree primary key (`traceId`, `spanId`) order by(`traceId`, `spanId`, `app_name`, `instance_name`, `parentSpanId`)
+;
+
 
 DROP TABLE IF EXISTS `bithon_event`;
 CREATE TABLE `bithon_event` (
@@ -94,31 +111,5 @@ CREATE TABLE `bithon_event` (
   KEY `idx_timestamp` (`timestamp`)
 )ENGINE=InnoDB AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8mb4 COMMENT='';
 
-DROP TABLE IF EXISTS `bithon_metric_dimension`;
-CREATE TABLE `bithon_metric_dimension` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `data_source` VARCHAR(64) NOT NULL COMMENT '',
-  `dimension_name` VARCHAR(64) NOT NULL COMMENT '',
-  `dimension_value` VARCHAR(256) NOT NULL COMMENT '',
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '',
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '',
-  PRIMARY KEY `idx_id` (`id`),
-  UNIQUE `idx_dimension` (`data_source`, `dimension_name`, `dimension_value`),
-  KEY `idx_created_at` (`created_at`),
-  KEY `idx_updated_at` (`updated_at`)
-) ENGINE=InnoDB AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8mb4 COMMENT='Metric Dimension';
-
-DROP TABLE IF EXISTS `bithon_application_topo`;
-CREATE TABLE `bithon_application_topo` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `src_endpoint` VARCHAR(64) NOT NULL COMMENT '',
-  `src_endpoint_type` VARCHAR(16) NOT NULL COMMENT '0:Application; 1:MySQL; 2:REDIS; 3:Mongo;...',
-  `dst_endpoint` VARCHAR(256) NOT NULL COMMENT '',
-  `dst_endpoint_type` VARCHAR(16) NOT NULL COMMENT '0:Application; 1:MySQL 2:REDIS; 3:Mongo;...',
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '',
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '',
-  PRIMARY KEY `idx_id` (`id`),
-  UNIQUE `idx_topo` (`src_endpoint`, `src_endpoint_type`, `dst_endpoint`, `dst_endpoint_type`),
-  KEY `idx_created_at` (`created_at`),
-  KEY `idx_updated_at` (`updated_at`)
-) ENGINE=InnoDB AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8mb4 COMMENT='TOPO';
+# ClickHouse
+CREATE TABLE `bithon_event` (`app_name` VARCHAR(64) NOT NULL ,`instance_name` VARCHAR(64) NOT NULL ,`type` VARCHAR(64) NOT NULL,`arguments` TEXT ,`timestamp` TIMESTAMP(3,0) NOT NULL) ENGINE=MergeTree order by (`app_name`, `instance_name`, `type`);
