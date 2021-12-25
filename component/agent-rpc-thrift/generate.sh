5ba3124b6debf0c22c54af52cb1927b15b09ceaf
Frank Chen <frank.chen021@outlook.com>
2021-12-25 17:43:13 +0800

    Remove thrift as message channel (#267)
    * Remove thrift as message channel
    
    * Remove from pom
    
    * Remove thrift impl of setting

--- component/agent-rpc-thrift/generate.sh
+++
@@ -1,4 +0,0 @@
-thrift -v -r --gen java:generated_annotations=suppress --out src/main/java src/main/thrift/IMetricCollector.thrift
-thrift -v -r --gen java:generated_annotations=suppress --out src/main/java src/main/thrift/SettingService.thrift
-thrift -v -r --gen java:generated_annotations=suppress --out src/main/java src/main/thrift/ITraceCollector.thrift
-thrift -v -r --gen java:generated_annotations=suppress --out src/main/java src/main/thrift/IEventCollector.thrift
