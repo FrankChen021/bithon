FrankChen021/bithon

commit 7c04da0ad7d8a9401493e3d3ce4fb91f832ce7db

Comply with interceptor naming specification (#269)

agent/agent-plugins/httpclient-jdk/src/main/java/org/bithon/agent/plugin/httpclient/jdk/JdkHttpClientPlugin.java
@@ -47,22 +47,22 @@ public class JdkHttpClientPlugin implements IPlugin {
                                                                  .and(takesArguments(5))
                                                                  .and(MatcherUtils.takesArgument(4,
                                                                                                  "sun.net.www.protocol.http.HttpURLConnection")))
-                                                   .to("org.bithon.agent.plugin.httpclient.jdk.HttpClientNewInterceptor"),
+                                                   .to("org.bithon.agent.plugin.httpclient.jdk.interceptor.HttpClient$New"),
 
                     MethodPointCutDescriptorBuilder.build()
                                                    .onMethodAndArgs("writeRequests", "sun.net.www.MessageHeader")
-                                                   .to("org.bithon.agent.plugin.httpclient.jdk.HttpClientWriteRequestInterceptor"),
+                                                   .to("org.bithon.agent.plugin.httpclient.jdk.interceptor.HttpClient$WriteRequest"),
 
                     MethodPointCutDescriptorBuilder.build()
                                                    .onMethodAndArgs("writeRequests",
                                                                     "sun.net.www.MessageHeader",
                                                                     "sun.net.www.http.PosterOutputStream")
-                                                   .to("org.bithon.agent.plugin.httpclient.jdk.HttpClientWriteRequestInterceptor"),
+                                                   .to("org.bithon.agent.plugin.httpclient.jdk.interceptor.HttpClient$WriteRequest"),
 
                     MethodPointCutDescriptorBuilder.build()
                                                    .onMethod(named("parseHTTP").and(MatcherUtils.takesArgument(0,
                                                                                                                "sun.net.www.MessageHeader")))
-                                                   .to("org.bithon.agent.plugin.httpclient.jdk.HttpClientParseHttpInterceptor")),
+                                                   .to("org.bithon.agent.plugin.httpclient.jdk.interceptor.HttpClient$ParseHttp")),
 
             // HttpsClient inherits from HttpClient
             forBootstrapClass("sun.net.www.protocol.https.HttpsClient")
@@ -76,7 +76,7 @@ public class JdkHttpClientPlugin implements IPlugin {
                                                                  .and(MatcherUtils.takesArgument(3, "java.net.Proxy"))
                                                                  .and(MatcherUtils.takesArgument(6,
                                                                                                  "sun.net.www.protocol.http.HttpURLConnection")))
-                                                   .to("org.bithon.agent.plugin.httpclient.jdk.HttpsClientNewInterceptor"))
+                                                   .to("org.bithon.agent.plugin.httpclient.jdk.interceptor.HttpsClient$New"))
 
         );
     }

agent/agent-plugins/httpclient-apache/src/main/java/org/bithon/agent/plugin/httpclient/apache/ApacheHttpClientPlugin.java
@@ -75,11 +75,11 @@ public class ApacheHttpClientPlugin implements IPlugin {
                                                                     "org.apache.http.HttpHost",
                                                                     "org.apache.http.HttpRequest",
                                                                     "org.apache.http.protocol.HttpContext")
-                                                   .to("org.bithon.agent.plugin.httpclient.apache.metrics.DefaultRequestDirectorExecute"),
+                                                   .to("org.bithon.agent.plugin.httpclient.apache.metrics.DefaultRequestDirector$Execute"),
 
                     MethodPointCutDescriptorBuilder.build()
                                                    .onMethodAndNoArgs("releaseConnection")
-                                                   .to("org.bithon.agent.plugin.httpclient.apache.metrics.DefaultRequestDirectorReleaseConnection")
+                                                   .to("org.bithon.agent.plugin.httpclient.apache.metrics.DefaultRequestDirector$ReleaseConnection")
                 ),
 
             //
@@ -92,7 +92,7 @@ public class ApacheHttpClientPlugin implements IPlugin {
                                                                     "org.apache.http.HttpRequest",
                                                                     "org.apache.http.HttpClientConnection",
                                                                     "org.apache.http.protocol.HttpContext")
-                                                   .to("org.bithon.agent.plugin.httpclient.apache.trace.HttpRequestInterceptor")
+                                                   .to("org.bithon.agent.plugin.httpclient.apache.interceptor.HttpRequestExecutor$Execute")
                 )
         );
     }

agent/agent-plugins/httpclient-apache/src/main/java/org/bithon/agent/plugin/httpclient/apache/interceptor/HttpRequestExecutor$Execute.java
@@ -14,47 +14,57 @@
  *    limitations under the License.
  */
 
-package org.bithon.agent.plugin.httpclient.jdk;
+package org.bithon.agent.plugin.httpclient.apache.interceptor;
 
+
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
 import org.bithon.agent.bootstrap.aop.AbstractInterceptor;
 import org.bithon.agent.bootstrap.aop.AopContext;
-import org.bithon.agent.bootstrap.aop.IBithonObject;
 import org.bithon.agent.bootstrap.aop.InterceptionDecision;
 import org.bithon.agent.core.tracing.context.ITraceSpan;
 import org.bithon.agent.core.tracing.context.SpanKind;
 import org.bithon.agent.core.tracing.context.Tags;
 import org.bithon.agent.core.tracing.context.TraceSpanFactory;
-import sun.net.www.MessageHeader;
-
-import java.net.HttpURLConnection;
 
 /**
  * @author frankchen
  */
-public class HttpClientWriteRequestInterceptor extends AbstractInterceptor {
+public class HttpRequestExecutor$Execute extends AbstractInterceptor {
 
     @Override
     public InterceptionDecision onMethodEnter(AopContext aopContext) {
-        MessageHeader headers = (MessageHeader) aopContext.getArgs()[0];
+        HttpRequest httpRequest = (HttpRequest) aopContext.getArgs()[0];
 
-        ITraceSpan span = TraceSpanFactory.newSpan("httpClient-jdk");
+        //
+        // Trace
+        //
+        ITraceSpan span = TraceSpanFactory.newSpan("httpClient");
         if (span == null) {
             return InterceptionDecision.SKIP_LEAVE;
         }
 
-        IBithonObject injectedObject = aopContext.castTargetAs();
-        HttpURLConnection connection = (HttpURLConnection) injectedObject.getInjectedObject();
-
-        /*
-         * starts a span which will be finished after HttpClient.parseHttp
-         */
+        // create a span and save it in user-context
         aopContext.setUserContext(span.method(aopContext.getMethod())
                                       .kind(SpanKind.CLIENT)
-                                      .tag(Tags.URI, connection.getURL().toString())
-                                      .tag(Tags.HTTP_METHOD, connection.getRequestMethod())
-                                      .propagate(headers, (headersArgs, key, value) -> headersArgs.set(key, value))
+                                      .tag(Tags.URI, httpRequest.getRequestLine().getUri())
+                                      .tag(Tags.HTTP_METHOD, httpRequest.getRequestLine().getMethod())
+                                      .propagate(httpRequest, (request, key, value) -> request.setHeader(key, value))
                                       .start());
 
         return InterceptionDecision.CONTINUE;
+    }
+
+    @Override
+    public void onMethodLeave(AopContext context) {
+        ITraceSpan thisSpan = context.castUserContextAs();
+        if (thisSpan == null) {
+            // in case of exception in above
+            return;
+        }
+
+        HttpResponse response = context.castReturningAs();
+        String status = response.getStatusLine() == null ? "-1" : Integer.toString(response.getStatusLine().getStatusCode());
+        thisSpan.tag(Tags.STATUS, status).tag(context.getException()).finish();
     }
 }

agent/agent-plugins/httpclient-apache/src/main/java/org/bithon/agent/plugin/httpclient/apache/metrics/DefaultRequestDirector$Execute.java
@@ -36,7 +36,7 @@ import org.bithon.agent.core.metric.domain.http.HttpOutgoingMetricsCollector;
  * @author frank.chen021@outlook.com
  * @date 2021/3/15
  */
-public class DefaultRequestDirectorExecute extends AbstractInterceptor {
+public class DefaultRequestDirector$Execute extends AbstractInterceptor {
 
     private HttpOutgoingMetricsCollector metricCollector;
 

agent/agent-plugins/httpclient-apache/src/main/java/org/bithon/agent/plugin/httpclient/apache/metrics/DefaultRequestDirector$ReleaseConnection.java
@@ -33,17 +33,19 @@ import shaded.org.slf4j.LoggerFactory;
 import java.lang.reflect.Field;
 
 /**
+ * {@link DefaultRequestDirector#releaseConnection()}
+ *
  * @author frank.chen021@outlook.com
  * @date 2021/3/15
  */
-public class DefaultRequestDirectorReleaseConnection extends AbstractInterceptor {
-    private static Logger log = LoggerFactory.getLogger(DefaultRequestDirectorReleaseConnection.class);
+public class DefaultRequestDirector$ReleaseConnection extends AbstractInterceptor {
+    private static Logger log = LoggerFactory.getLogger(DefaultRequestDirector$ReleaseConnection.class);
 
     private HttpOutgoingMetricsCollector metricProvider;
     private Field managedConnectionField;
 
     @Override
-    public boolean initialize() throws Exception {
+    public boolean initialize() {
         metricProvider = MetricCollectorManager.getInstance()
                                                .getOrRegister("apache-http-client", HttpOutgoingMetricsCollector.class);
 

agent/agent-plugins/httpclient-jdk/src/main/java/org/bithon/agent/plugin/httpclient/jdk/interceptor/HttpClient$New.java
@@ -14,21 +14,26 @@
  *    limitations under the License.
  */
 
-package org.bithon.agent.plugin.httpclient.jdk;
+package org.bithon.agent.plugin.httpclient.jdk.interceptor;
 
 import org.bithon.agent.bootstrap.aop.AbstractInterceptor;
 import org.bithon.agent.bootstrap.aop.AopContext;
 import org.bithon.agent.bootstrap.aop.IBithonObject;
+import sun.net.www.protocol.http.HttpURLConnection;
+
+import java.net.Proxy;
+import java.net.URL;
 
 /**
+ * {@link sun.net.www.http.HttpClient#New(URL, Proxy, int, boolean, HttpURLConnection)}
+ *
  * @author frank.chen021@outlook.com
- * @date 2021/3/14 11:13 下午
+ * @date 2021/2/21 11:13 下午
  */
-public class HttpsClientNewInterceptor extends AbstractInterceptor {
+public class HttpClient$New extends AbstractInterceptor {
     /**
      * inject HttpURLConnection instance, which creates HttpClient instance, into the instance of HttpClient as its parent
      *
-     * @param aopContext
      */
     @Override
     public void onMethodLeave(AopContext aopContext) {
@@ -38,6 +43,6 @@ public class HttpsClientNewInterceptor extends AbstractInterceptor {
             return;
         }
 
-        injectedObject.setInjectedObject(aopContext.getArgs()[6]);
+        injectedObject.setInjectedObject(aopContext.getArgs()[4]);
     }
 }

agent/agent-plugins/httpclient-jdk/src/main/java/org/bithon/agent/plugin/httpclient/jdk/interceptor/HttpClient$ParseHttp.java
@@ -14,7 +14,7 @@
  *    limitations under the License.
  */
 
-package org.bithon.agent.plugin.httpclient.jdk;
+package org.bithon.agent.plugin.httpclient.jdk.interceptor;
 
 import org.bithon.agent.bootstrap.aop.AbstractInterceptor;
 import org.bithon.agent.bootstrap.aop.AopContext;
@@ -31,7 +31,7 @@ import sun.net.www.protocol.http.HttpURLConnection;
 /**
  * @author frankchen
  */
-public class HttpClientParseHttpInterceptor extends AbstractInterceptor {
+public class HttpClient$ParseHttp extends AbstractInterceptor {
 
     //TODO: jdk-http metrics
     HttpOutgoingMetricsCollector metricCollector;

agent/agent-plugins/httpclient-jdk/src/main/java/org/bithon/agent/plugin/httpclient/jdk/interceptor/HttpClient$WriteRequest.java
@@ -14,56 +14,47 @@
  *    limitations under the License.
  */
 
-package org.bithon.agent.plugin.httpclient.apache.trace;
+package org.bithon.agent.plugin.httpclient.jdk.interceptor;
 
-
-import org.apache.http.HttpRequest;
-import org.apache.http.HttpResponse;
 import org.bithon.agent.bootstrap.aop.AbstractInterceptor;
 import org.bithon.agent.bootstrap.aop.AopContext;
+import org.bithon.agent.bootstrap.aop.IBithonObject;
 import org.bithon.agent.bootstrap.aop.InterceptionDecision;
 import org.bithon.agent.core.tracing.context.ITraceSpan;
 import org.bithon.agent.core.tracing.context.SpanKind;
 import org.bithon.agent.core.tracing.context.Tags;
 import org.bithon.agent.core.tracing.context.TraceSpanFactory;
+import sun.net.www.MessageHeader;
+
+import java.net.HttpURLConnection;
 
 /**
  * @author frankchen
  */
-public class HttpRequestInterceptor extends AbstractInterceptor {
+public class HttpClient$WriteRequest extends AbstractInterceptor {
 
     @Override
     public InterceptionDecision onMethodEnter(AopContext aopContext) {
-        HttpRequest httpRequest = (HttpRequest) aopContext.getArgs()[0];
+        MessageHeader headers = (MessageHeader) aopContext.getArgs()[0];
 
-        //
-        // Trace
-        //
-        ITraceSpan span = TraceSpanFactory.newSpan("httpClient");
+        ITraceSpan span = TraceSpanFactory.newSpan("httpClient-jdk");
         if (span == null) {
             return InterceptionDecision.SKIP_LEAVE;
         }
 
-        // create a span and save it in user-context
+        IBithonObject injectedObject = aopContext.castTargetAs();
+        HttpURLConnection connection = (HttpURLConnection) injectedObject.getInjectedObject();
+
+        /*
+         * starts a span which will be finished after HttpClient.parseHttp
+         */
         aopContext.setUserContext(span.method(aopContext.getMethod())
                                       .kind(SpanKind.CLIENT)
-                                      .tag(Tags.URI, httpRequest.getRequestLine().getUri())
-                                      .tag(Tags.HTTP_METHOD, httpRequest.getRequestLine().getMethod())
-                                      .propagate(httpRequest, (request, key, value) -> request.setHeader(key, value))
+                                      .tag(Tags.URI, connection.getURL().toString())
+                                      .tag(Tags.HTTP_METHOD, connection.getRequestMethod())
+                                      .propagate(headers, (headersArgs, key, value) -> headersArgs.set(key, value))
                                       .start());
 
         return InterceptionDecision.CONTINUE;
-    }
-
-    @Override
-    public void onMethodLeave(AopContext context) {
-        ITraceSpan thisSpan = context.castUserContextAs();
-        if (thisSpan == null) {
-            return;
-        }
-
-        HttpResponse response = context.castReturningAs();
-        String status = response.getStatusLine() == null ? "-1" : Integer.toString(response.getStatusLine().getStatusCode());
-        thisSpan.tag("status", status).tag(context.getException()).finish();
     }
 }

commit 37ef4def4bf8807ddb98c7d0a9033bd87f5976e5

Fix bug caused by #269

agent/agent-plugins/httpclient-jdk/src/main/java/org/bithon/agent/plugin/httpclient/jdk/JdkHttpClientPlugin.java
@@ -46,21 +46,21 @@ public class JdkHttpClientPlugin implements IPlugin {
                                                                  .and(isStatic())
                                                                  .and(takesArguments(5))
                                                                  .and(Matchers.takesArgument(4, "sun.net.www.protocol.http.HttpURLConnection")))
-                                                   .to("org.bithon.agent.plugin.httpclient.jdk.HttpClientNewInterceptor"),
+                                                   .to("org.bithon.agent.plugin.httpclient.jdk.interceptor.HttpClient$New"),
 
                     MethodPointCutDescriptorBuilder.build()
                                                    .onMethodAndArgs("writeRequests", "sun.net.www.MessageHeader")
-                                                   .to("org.bithon.agent.plugin.httpclient.jdk.HttpClientWriteRequestInterceptor"),
+                                                   .to("org.bithon.agent.plugin.httpclient.jdk.interceptor.HttpClient$WriteRequest"),
 
                     MethodPointCutDescriptorBuilder.build()
                                                    .onMethodAndArgs("writeRequests",
                                                                     "sun.net.www.MessageHeader",
                                                                     "sun.net.www.http.PosterOutputStream")
-                                                   .to("org.bithon.agent.plugin.httpclient.jdk.HttpClientWriteRequestInterceptor"),
+                                                   .to("org.bithon.agent.plugin.httpclient.jdk.interceptor.HttpClient$WriteRequest"),
 
                     MethodPointCutDescriptorBuilder.build()
                                                    .onMethod(named("parseHTTP").and(Matchers.takesArgument(0, "sun.net.www.MessageHeader")))
-                                                   .to("org.bithon.agent.plugin.httpclient.jdk.HttpClientParseHttpInterceptor")),
+                                                   .to("org.bithon.agent.plugin.httpclient.jdk.interceptor.HttpClient$ParseHttp")),
 
             // HttpsClient inherits from HttpClient
             forBootstrapClass("sun.net.www.protocol.https.HttpsClient")
@@ -74,7 +74,7 @@ public class JdkHttpClientPlugin implements IPlugin {
                                                                  .and(Matchers.takesArgument(3, "java.net.Proxy"))
                                                                  .and(Matchers.takesArgument(6,
                                                                                              "sun.net.www.protocol.http.HttpURLConnection")))
-                                                   .to("org.bithon.agent.plugin.httpclient.jdk.HttpsClientNewInterceptor"))
+                                                   .to("org.bithon.agent.plugin.httpclient.jdk.interceptor.HttpsClient$New"))
 
         );
     }