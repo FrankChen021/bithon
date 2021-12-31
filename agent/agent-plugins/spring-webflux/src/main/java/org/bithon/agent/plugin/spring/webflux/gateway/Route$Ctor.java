c943cae08fb803e12d04792892b8f68f48bdcf32
frank chen <frank.chen021@outlook.com>
2021-12-31 13:59:28 +0800

    Prevent repeated instrumentation in early stage

--- agent/agent-plugins/spring-webflux/src/main/java/org/bithon/agent/plugin/spring/webflux/gateway/Route$Ctor.java
+++ agent/agent-plugins/spring-webflux/src/main/java/org/bithon/agent/plugin/spring/webflux/gateway/Route$Ctor.java
@@ -21,9 +21,13 @@ import org.bithon.agent.bootstrap.aop.AopContext;
 import org.bithon.agent.core.utils.CollectionUtils;
 import org.bithon.agent.plugin.spring.webflux.gateway.aop.GatewayAopDynamicInstaller;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
 import org.springframework.cloud.gateway.route.Route;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
 
 /**
  * Install handler to user gateway filter
@@ -32,6 +36,8 @@ import java.util.List;
  * @date 30/12/21 2:16 PM
  */
 public class Route$Ctor extends AbstractInterceptor {
+
+    private final Set<String> clazz = new ConcurrentSkipListSet<>();
 
     @Override
     public void onConstruct(AopContext aopContext) {
@@ -45,6 +51,21 @@ public class Route$Ctor extends AbstractInterceptor {
             return;
         }
 
-        GatewayAopDynamicInstaller.install(filters.stream().map(GatewayFilter::getClass));
+        List<Class<?>> filterClassList = new ArrayList<>();
+        for (GatewayFilter filter : filters) {
+            Class<?> filterClass;
+            if (filter instanceof OrderedGatewayFilter) {
+                filterClass = ((OrderedGatewayFilter) filter).getDelegate().getClass();
+            } else {
+                filterClass = filter.getClass();
+            }
+            if (clazz.add(filterClass.getName())) {
+                filterClassList.add(filterClass);
+            }
+        }
+
+        if (!filterClassList.isEmpty()) {
+            GatewayAopDynamicInstaller.install(filterClassList.stream());
+        }
     }
 }
