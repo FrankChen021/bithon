6950212cf5f28025643c2d6d45ec9fe1c345d77f
frank chen <frank.chen021@outlook.com>
2022-01-04 23:16:33 +0800

    Add Apache Druid plugin to get query

---
+++ agent/agent-plugins/apache-druid/src/main/java/org/bithon/agent/plugin/apache/druid/interceptor/QueryLifecycle$Initialize.java
@@ -0,0 +1,76 @@
+/*
+ *    Copyright 2020 bithon.org
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.bithon.agent.plugin.apache.druid.interceptor;
+
+import org.apache.druid.server.QueryLifecycle;
+import org.bithon.agent.bootstrap.aop.AbstractInterceptor;
+import org.bithon.agent.bootstrap.aop.AopContext;
+import org.bithon.agent.bootstrap.aop.InterceptionDecision;
+import org.bithon.agent.core.tracing.context.ITraceContext;
+import org.bithon.agent.core.tracing.context.TraceContextHolder;
+import org.bithon.component.commons.logging.ILogAdaptor;
+import org.bithon.component.commons.logging.LoggerFactory;
+import shaded.com.fasterxml.jackson.core.JsonProcessingException;
+import shaded.com.fasterxml.jackson.databind.ObjectMapper;
+import shaded.com.fasterxml.jackson.databind.SerializationFeature;
+import shaded.com.fasterxml.jackson.datatype.joda.JodaModule;
+
+/**
+ * @author Frank Chen
+ * @date 4/1/22 6:38 PM
+ */
+public class QueryLifecycle$Initialize extends AbstractInterceptor {
+
+    private static final ILogAdaptor log = LoggerFactory.getLogger(QueryLifecycle$Initialize.class);
+
+    private final ObjectMapper om;
+
+    public QueryLifecycle$Initialize() {
+        this.om = new ObjectMapper().configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
+                                    .configure(SerializationFeature.INDENT_OUTPUT, true);
+        /*
+          NOTE
+          JodaModule is defined in com.fasterxml.jackson.datatype:jackson-datatype-joda and is shaded in shaded-jackson module
+          but the org.joda.time.* classes in that module is excluded from the shaded module
+          This is because that package is not shaded, we want to use these class loaded in user's class loader to do serialization.
+          These classes are used by the Query object below.
+
+          This is not the best way because we can't use the shaded jackson to serialize/deserialize shaded joda objects anymore.
+         */
+        this.om.registerModule(new JodaModule());
+    }
+
+    @Override
+    public InterceptionDecision onMethodEnter(AopContext aopContext) {
+        return InterceptionDecision.CONTINUE;
+    }
+
+    @Override
+    public void onMethodLeave(AopContext aopContext) {
+        ITraceContext ctx = TraceContextHolder.current();
+        if (ctx != null) {
+            QueryLifecycle lifecycle = aopContext.castTargetAs();
+            if (!ctx.currentSpan().tags().containsKey("query")) {
+                try {
+                    ctx.currentSpan().tag("query", om.writeValueAsString(lifecycle.getQuery()));
+                } catch (JsonProcessingException e) {
+                    log.error("Unable to serialize query object: {}", e.getMessage());
+                }
+            }
+        }
+    }
+}
---
+++ agent/agent-plugins/apache-druid/src/main/java/org/bithon/agent/plugin/apache/druid/interceptor/SqlLifecycle$Initialize.java
@@ -0,0 +1,42 @@
+/*
+ *    Copyright 2020 bithon.org
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.bithon.agent.plugin.apache.druid.interceptor;
+
+import org.bithon.agent.bootstrap.aop.AbstractInterceptor;
+import org.bithon.agent.bootstrap.aop.AopContext;
+import org.bithon.agent.bootstrap.aop.InterceptionDecision;
+import org.bithon.agent.core.tracing.context.ITraceContext;
+import org.bithon.agent.core.tracing.context.TraceContextHolder;
+
+/**
+ * @author Frank Chen
+ * @date 4/1/22 6:37 PM
+ */
+public class SqlLifecycle$Initialize extends AbstractInterceptor {
+
+    @Override
+    public InterceptionDecision onMethodEnter(AopContext aopContext) {
+        ITraceContext ctx = TraceContextHolder.current();
+        if (ctx != null) {
+            Object query = aopContext.getArgs()[0];
+            if (query != null && !ctx.currentSpan().tags().containsKey("query")) {
+                ctx.currentSpan().tag("query", query.toString());
+            }
+        }
+        return InterceptionDecision.SKIP_LEAVE;
+    }
+}
