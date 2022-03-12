68f16dfcfa5975d73c262f61603fd93f82a99c23
Frank Chen <frank.chen021@outlook.com>
2022-03-12 16:51:11 +0800

    Remove bootstrap related code (#346)
    Signed-off-by: frank chen <frank.chen021@outlook.com>

--- agent/agent-plugins/httpclient-jdk/src/main/java/org/bithon/agent/plugin/httpclient/jdk/JdkHttpClientPlugin.java
+++ agent/agent-plugins/httpclient-jdk/src/main/java/org/bithon/agent/plugin/httpclient/jdk/JdkHttpClientPlugin.java
@@ -24,7 +24,7 @@ import org.bithon.agent.core.plugin.IPlugin;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.bithon.agent.core.aop.descriptor.InterceptorDescriptorBuilder.forBootstrapClass;
+import static org.bithon.agent.core.aop.descriptor.InterceptorDescriptorBuilder.forClass;
 import static shaded.net.bytebuddy.matcher.ElementMatchers.isStatic;
 import static shaded.net.bytebuddy.matcher.ElementMatchers.named;
 import static shaded.net.bytebuddy.matcher.ElementMatchers.takesArguments;
@@ -39,7 +39,7 @@ public class JdkHttpClientPlugin implements IPlugin {
     @Override
     public List<InterceptorDescriptor> getInterceptors() {
         return Arrays.asList(
-            forBootstrapClass("sun.net.www.http.HttpClient")
+            forClass("sun.net.www.http.HttpClient")
                 .methods(
                     MethodPointCutDescriptorBuilder.build()
                                                    .onMethod(named("New")
@@ -63,7 +63,7 @@ public class JdkHttpClientPlugin implements IPlugin {
                                                    .to("org.bithon.agent.plugin.httpclient.jdk.interceptor.HttpClient$ParseHttp")),
 
             // HttpsClient inherits from HttpClient
-            forBootstrapClass("sun.net.www.protocol.https.HttpsClient")
+            forClass("sun.net.www.protocol.https.HttpsClient")
                 .methods(
                     MethodPointCutDescriptorBuilder.build()
                                                    .onMethod(named("New")
