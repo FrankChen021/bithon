fd6472bfa86bc963d44ea65d8dc5935ef729cf0f
Frank Chen <frank.chen021@outlook.com>
2022-08-21 19:48:50 +0800

    Remove unused

--- agent/agent-plugins/httpclient-jdk/src/main/java/org/bithon/agent/plugin/httpclient/jdk/interceptor/NetworkClient$DoConnect.java
+++ agent/agent-plugins/httpclient-jdk/src/main/java/org/bithon/agent/plugin/httpclient/jdk/interceptor/NetworkClient$DoConnect.java
@@ -20,15 +20,6 @@ import org.bithon.agent.bootstrap.aop.AbstractInterceptor;
 import org.bithon.agent.bootstrap.aop.AopContext;
 import org.bithon.agent.bootstrap.aop.IBithonObject;
 
-import java.io.FilterInputStream;
-import java.io.FilterOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-import java.net.SocketException;
-import java.net.SocketImpl;
-
 /**
  * {@link sun.net.NetworkClient#doConnect(String, int)}
  *
@@ -58,44 +49,14 @@ public class NetworkClient$DoConnect extends AbstractInterceptor {
 
         final HttpClientContext clientContext = (HttpClientContext) bithonObject.getInjectedObject();
 
+        //
+        // doConnect returns an object of java.net.Socket
+        // we need to inject the context to this object
+        // So that corresponding interceptors of Socket can handle IO statistics
+        //
         Object socket = aopContext.getReturning();
         if (socket instanceof IBithonObject) {
             ((IBithonObject) socket).setInjectedObject(clientContext);
-        }
-    }
-
-    static class SocketProxy extends Socket {
-
-        private final HttpClientContext clientContext;
-
-        SocketProxy(SocketImpl impl, HttpClientContext clientContext) throws SocketException {
-            super(impl);
-            this.clientContext = clientContext;
-        }
-
-        @Override
-        public InputStream getInputStream() throws IOException {
-            InputStream is = super.getInputStream();
-            return new FilterInputStream(is) {
-                @Override
-                public int read(byte[] b, int off, int len) throws IOException {
-                    int size = super.read(b, off, len);
-                    clientContext.getReceiveBytes().update(size);
-                    return size;
-                }
-            };
-        }
-
-        @Override
-        public OutputStream getOutputStream() throws IOException {
-            OutputStream os = super.getOutputStream();
-            return new FilterOutputStream(os) {
-                @Override
-                public void write(byte[] b, int off, int len) throws IOException {
-                    super.write(b, off, len);
-                    clientContext.getSentBytes().update(len);
-                }
-            };
         }
     }
 }
