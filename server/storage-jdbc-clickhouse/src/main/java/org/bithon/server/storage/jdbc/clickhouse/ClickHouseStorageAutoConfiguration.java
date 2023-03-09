6ce5cbff8c2748b39a99d16e3903828655dd1fa9
frank chen <frank.chen021@outlook.com>
2023-03-09 17:35:55 +0800

    Improve CK exception translation

--- server/storage-jdbc-clickhouse/src/main/java/org/bithon/server/storage/jdbc/clickhouse/ClickHouseStorageAutoConfiguration.java
+++ server/storage-jdbc-clickhouse/src/main/java/org/bithon/server/storage/jdbc/clickhouse/ClickHouseStorageAutoConfiguration.java
@@ -20,8 +20,11 @@ import com.alibaba.druid.pool.DruidDataSource;
 import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.databind.Module;
 import org.bithon.server.storage.jdbc.clickhouse.web.DashboardStorage;
+import org.jooq.ExecuteContext;
 import org.jooq.impl.DSL;
 import org.jooq.impl.DefaultConfiguration;
+import org.jooq.impl.DefaultExecuteListener;
+import org.jooq.impl.DefaultExecuteListenerProvider;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jooq.JooqAutoConfiguration;
@@ -31,6 +34,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
+import java.sql.SQLException;
 
 /**
  * @author frank.chen021@outlook.com
@@ -62,7 +66,18 @@ public class ClickHouseStorageAutoConfiguration {
         JooqAutoConfiguration autoConfiguration = new JooqAutoConfiguration();
         return new ClickHouseJooqContextHolder(DSL.using(new DefaultConfiguration().set(autoConfiguration.dataSourceConnectionProvider(dataSource))
                                                                                    .set(new JooqProperties().determineSqlDialect(dataSource))
-                                                                                   .set(autoConfiguration.jooqExceptionTranslatorExecuteListenerProvider())));
+                                                                                   .set(new DefaultExecuteListenerProvider(new ClickHouseExceptionTranslator()))));
+    }
+
+    static class ClickHouseExceptionTranslator extends DefaultExecuteListener {
+        @Override
+        public void exception(ExecuteContext ctx) {
+            SQLException exception = ctx.sqlException();
+            if (exception == null) {
+                return;
+            }
+            ctx.exception(new ClickHouseAccessException(exception.getMessage(), exception.getCause()));
+        }
     }
 
     @Bean
