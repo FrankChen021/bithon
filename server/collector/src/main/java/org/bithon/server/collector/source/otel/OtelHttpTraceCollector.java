b8564343ad182c00736bae492b6b64ba0c1274ff
Frank Chen <frank.chen021@outlook.com>
2023-09-02 11:31:54 +0800

    Fix message converter

--- server/collector/src/main/java/org/bithon/server/collector/source/otel/OtelHttpTraceCollector.java
+++ server/collector/src/main/java/org/bithon/server/collector/source/otel/OtelHttpTraceCollector.java
@@ -16,21 +16,9 @@
 
 package org.bithon.server.collector.source.otel;
 
-import com.google.protobuf.CodedInputStream;
-import com.google.protobuf.ExtensionRegistryLite;
-import com.google.protobuf.util.JsonFormat;
-import io.opentelemetry.proto.common.v1.KeyValue;
-import io.opentelemetry.proto.trace.v1.ResourceSpans;
-import io.opentelemetry.proto.trace.v1.ScopeSpans;
-import io.opentelemetry.proto.trace.v1.Span;
-import io.opentelemetry.proto.trace.v1.Status;
 import lombok.extern.slf4j.Slf4j;
-import org.bithon.component.commons.tracing.SpanKind;
-import org.bithon.component.commons.tracing.Tags;
-import org.bithon.component.commons.utils.CollectionUtils;
 import org.bithon.component.commons.utils.StringUtils;
 import org.bithon.server.sink.tracing.ITraceMessageSink;
-import org.bithon.server.storage.tracing.TraceSpan;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.HttpStatus;
@@ -41,13 +29,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.InflaterInputStream;
 
@@ -87,11 +68,11 @@ public class OtelHttpTraceCollector {
             is = request.getInputStream();
         }
 
-        ResourceSpans resourceSpans;
+        SpanConverter spanConverter;
         if ("application/x-protobuf".equals(request.getContentType())) {
-            resourceSpans = fromBinary(is);
+            spanConverter = SpanConverter.fromBinary(is);
         } else if ("application/json".equals(request.getContentType())) {
-            resourceSpans = fromJson(is);
+            spanConverter = SpanConverter.fromJson(is);
         } else {
             String message = StringUtils.format("Not supported Content-Type [%s] from remote [%s]", request.getContentType(), request.getRemoteAddr());
             response.getWriter().println(message);
@@ -99,95 +80,7 @@ public class OtelHttpTraceCollector {
             return;
         }
 
-        List<TraceSpan> spans = new ArrayList<>(32);
-        for (ScopeSpans scopeSpans : resourceSpans.getScopeSpansList()) {
-            for (Span span : scopeSpans.getSpansList()) {
-                spans.add(toInternalSpan(scopeSpans, span));
-            }
-        }
-
-        if (CollectionUtils.isEmpty(spans)) {
-            return;
-        }
-
-        this.traceSink.process("trace", spans);
-    }
-
-    private ResourceSpans fromJson(InputStream is) throws IOException {
-        ResourceSpans.Builder builder = ResourceSpans.newBuilder();
-
-        try (InputStreamReader reader = new InputStreamReader(is, StandardCharsets.UTF_8)) {
-            JsonFormat.parser().merge(reader, builder);
-        }
-
-        return builder.build();
-    }
-
-    private ResourceSpans fromBinary(InputStream is) throws IOException {
-        ResourceSpans.Builder builder = ResourceSpans.newBuilder();
-
-        CodedInputStream.newInstance(is)
-                        .readMessage(builder,
-                                     ExtensionRegistryLite.getEmptyRegistry());
-
-        return builder.build();
-    }
-
-    private TraceSpan toInternalSpan(ScopeSpans scopeSpans, Span span) {
-        TraceSpan internalSpan = new TraceSpan();
-        internalSpan.traceId = span.getTraceId().toStringUtf8();
-        internalSpan.spanId = span.getSpanId().toStringUtf8();
-        internalSpan.parentSpanId = span.getParentSpanId().toStringUtf8();
-        internalSpan.kind = toSpanKind(span.getKind()).toString();
-        internalSpan.name = span.getName();
-        internalSpan.setTags(toAttributeMap(span.getAttributesList()));
-        internalSpan.setStartTime(span.getStartTimeUnixNano() / 1000);
-        internalSpan.setEndTime(span.getEndTimeUnixNano() / 1000);
-        internalSpan.setStatus(toStatus(span.getStatus()));
-        if (StringUtils.hasText(span.getStatus().getMessage())) {
-            internalSpan.setTag(Tags.Exception.MESSAGE, span.getStatus().getMessage());
-        }
-
-        internalSpan.appName = scopeSpans.getScope().getName();
-
-        return internalSpan;
-    }
-
-    private String toStatus(Status status) {
-        if (status.getCode() == Status.StatusCode.STATUS_CODE_OK) {
-            return "200";
-        }
-        if (status.getCode() == Status.StatusCode.STATUS_CODE_ERROR) {
-            return "500";
-        }
-        return "";
-    }
-
-    private Map<String, String> toAttributeMap(List<KeyValue> attributes) {
-        if (CollectionUtils.isEmpty(attributes)) {
-            return Collections.emptyMap();
-        }
-        Map<String, String> maps = new TreeMap<>();
-        attributes.forEach((kv) -> maps.put(kv.getKey(), kv.getValue().getStringValue()));
-        return maps;
-    }
-
-    private SpanKind toSpanKind(Span.SpanKind kind) {
-        switch (kind) {
-            case SPAN_KIND_INTERNAL:
-                return SpanKind.INTERNAL;
-            case SPAN_KIND_CLIENT:
-                return SpanKind.CLIENT;
-            case SPAN_KIND_CONSUMER:
-                return SpanKind.CONSUMER;
-            case SPAN_KIND_PRODUCER:
-                return SpanKind.PRODUCER;
-            case SPAN_KIND_SERVER:
-                return SpanKind.SERVER;
-            case SPAN_KIND_UNSPECIFIED:
-                return SpanKind.UNSPECIFIED;
-            default:
-                throw new UnsupportedOperationException("Not supported kind " + kind);
-        }
+        this.traceSink.process("trace",
+                               spanConverter.toSpanList());
     }
 }
---
+++ server/collector/src/main/java/org/bithon/server/collector/source/otel/SpanConverter.java
@@ -0,0 +1,177 @@
+/*
+ *    Copyright 2020 bithon.org
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.bithon.server.collector.source.otel;
+
+import com.google.common.io.BaseEncoding;
+import com.google.protobuf.ByteString;
+import com.google.protobuf.CodedInputStream;
+import com.google.protobuf.ExtensionRegistryLite;
+import com.google.protobuf.util.JsonFormat;
+import io.opentelemetry.proto.common.v1.KeyValue;
+import io.opentelemetry.proto.trace.v1.ResourceSpans;
+import io.opentelemetry.proto.trace.v1.ScopeSpans;
+import io.opentelemetry.proto.trace.v1.Span;
+import io.opentelemetry.proto.trace.v1.Status;
+import io.opentelemetry.proto.trace.v1.TracesData;
+import org.bithon.component.commons.tracing.SpanKind;
+import org.bithon.component.commons.tracing.Tags;
+import org.bithon.component.commons.utils.CollectionUtils;
+import org.bithon.component.commons.utils.StringUtils;
+import org.bithon.server.storage.tracing.TraceSpan;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * @author frank.chen021@outlook.com
+ * @date 2023/9/2 11:12
+ */
+class SpanConverter {
+
+    public static SpanConverter fromJson(InputStream is) throws IOException {
+        TracesData.Builder builder = TracesData.newBuilder();
+
+        try (InputStreamReader reader = new InputStreamReader(is, StandardCharsets.UTF_8)) {
+            JsonFormat.parser().merge(reader, builder);
+        }
+
+        return new SpanConverter(builder.build().getResourceSpansList()) {
+            @Override
+            protected String getTraceId(ByteString id) {
+                return BaseEncoding.base64().encode(id.toByteArray()).toLowerCase(Locale.ENGLISH);
+            }
+
+            @Override
+            protected String getSpanId(ByteString id) {
+                return BaseEncoding.base64().encode(id.toByteArray()).toLowerCase(Locale.ENGLISH);
+            }
+        };
+    }
+
+    public static SpanConverter fromBinary(InputStream is) throws IOException {
+        TracesData.Builder builder = TracesData.newBuilder();
+
+        CodedInputStream.newInstance(is)
+                        .readMessage(builder,
+                                     ExtensionRegistryLite.getEmptyRegistry());
+
+        return new SpanConverter(builder.build().getResourceSpansList());
+    }
+
+    private final List<ResourceSpans> resourceSpansList;
+
+    SpanConverter(List<ResourceSpans> resourceSpansList) {
+        this.resourceSpansList = resourceSpansList;
+    }
+
+    public List<TraceSpan> toSpanList() {
+        List<TraceSpan> spans = new ArrayList<>(32);
+        for (ResourceSpans resourceSpans : resourceSpansList) {
+            Map<String, String> resourceAttributes = toAttributeMap(resourceSpans.getResource().getAttributesList());
+
+            for (ScopeSpans scopeSpans : resourceSpans.getScopeSpansList()) {
+                for (Span span : scopeSpans.getSpansList()) {
+                    spans.add(toInternalSpan(resourceAttributes, scopeSpans, span));
+                }
+            }
+        }
+        return spans;
+    }
+
+    protected String getTraceId(ByteString id) {
+        return id.toStringUtf8();
+    }
+
+    protected String getSpanId(ByteString id) {
+        return id.toStringUtf8();
+    }
+
+    private TraceSpan toInternalSpan(Map<String, String> resourceAttributes,
+                                     ScopeSpans scopeSpans,
+                                     Span span) {
+        TraceSpan internalSpan = new TraceSpan();
+
+        internalSpan.traceId = getTraceId(span.getTraceId());
+        internalSpan.spanId = getSpanId(span.getSpanId());
+        internalSpan.parentSpanId = getSpanId(span.getParentSpanId());
+
+        internalSpan.kind = toSpanKind(span.getKind()).toString();
+        internalSpan.name = span.getName();
+
+        internalSpan.setTags(toAttributeMap(span.getAttributesList()));
+        internalSpan.setStartTime(span.getStartTimeUnixNano() / 1000);
+        internalSpan.setEndTime(span.getEndTimeUnixNano() / 1000);
+        internalSpan.setCostTime(internalSpan.endTime - internalSpan.startTime);
+        internalSpan.setStatus(toStatus(span.getStatus()));
+        if (StringUtils.hasText(span.getStatus().getMessage())) {
+            internalSpan.setTag(Tags.Exception.MESSAGE, span.getStatus().getMessage());
+        }
+
+        internalSpan.appName = resourceAttributes.getOrDefault("service.name",
+                                                               scopeSpans.getScope().getName());
+        internalSpan.instanceName = "";
+        internalSpan.method = "";
+
+        return internalSpan;
+    }
+
+    private String toStatus(Status status) {
+        if (status.getCode() == Status.StatusCode.STATUS_CODE_OK) {
+            return "200";
+        }
+        if (status.getCode() == Status.StatusCode.STATUS_CODE_ERROR) {
+            return "500";
+        }
+        return "";
+    }
+
+    private Map<String, String> toAttributeMap(List<KeyValue> attributes) {
+        if (CollectionUtils.isEmpty(attributes)) {
+            return Collections.emptyMap();
+        }
+        Map<String, String> maps = new TreeMap<>();
+        attributes.forEach((kv) -> maps.put(kv.getKey(), kv.getValue().getStringValue()));
+        return maps;
+    }
+
+    private SpanKind toSpanKind(Span.SpanKind kind) {
+        switch (kind) {
+            case SPAN_KIND_INTERNAL:
+                return SpanKind.INTERNAL;
+            case SPAN_KIND_CLIENT:
+                return SpanKind.CLIENT;
+            case SPAN_KIND_CONSUMER:
+                return SpanKind.CONSUMER;
+            case SPAN_KIND_PRODUCER:
+                return SpanKind.PRODUCER;
+            case SPAN_KIND_SERVER:
+                return SpanKind.SERVER;
+            case SPAN_KIND_UNSPECIFIED:
+                return SpanKind.UNSPECIFIED;
+            default:
+                throw new UnsupportedOperationException("Not supported kind " + kind);
+        }
+    }
+}
