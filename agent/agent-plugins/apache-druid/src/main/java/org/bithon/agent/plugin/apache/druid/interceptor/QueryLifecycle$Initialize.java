fc3121c256d151f45ce94d3cf31fc497ed5d2c8c
Frank Chen <frank.chen021@outlook.com>
2023-09-04 14:04:05 +0800

    Use toString to replace Json serialization

--- agent/agent-plugins/apache-druid/src/main/java/org/bithon/agent/plugin/apache/druid/interceptor/QueryLifecycle$Initialize.java
+++ agent/agent-plugins/apache-druid/src/main/java/org/bithon/agent/plugin/apache/druid/interceptor/QueryLifecycle$Initialize.java
@@ -21,11 +21,7 @@ import org.bithon.agent.instrumentation.aop.context.AopContext;
 import org.bithon.agent.instrumentation.aop.interceptor.declaration.AfterInterceptor;
 import org.bithon.agent.observability.tracing.context.ITraceContext;
 import org.bithon.agent.observability.tracing.context.TraceContextHolder;
-import org.bithon.component.commons.logging.LoggerFactory;
-import org.bithon.shaded.com.fasterxml.jackson.core.JsonProcessingException;
-import org.bithon.shaded.com.fasterxml.jackson.databind.ObjectMapper;
-import org.bithon.shaded.com.fasterxml.jackson.databind.SerializationFeature;
-import org.bithon.shaded.com.fasterxml.jackson.datatype.joda.JodaModule;
+import org.bithon.component.commons.tracing.Tags;
 
 /**
  * @author frank.chen021@outlook.com
@@ -33,36 +29,15 @@ import org.bithon.shaded.com.fasterxml.jackson.datatype.joda.JodaModule;
  */
 public class QueryLifecycle$Initialize extends AfterInterceptor {
 
-    private final ObjectMapper om;
-
-    public QueryLifecycle$Initialize() {
-        this.om = new ObjectMapper().configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
-                                    .configure(SerializationFeature.INDENT_OUTPUT, true);
-        /*
-          NOTE
-          JodaModule is defined in com.fasterxml.jackson.datatype:jackson-datatype-joda and is shaded in shaded-jackson module
-          but the org.joda.time.* classes in that module is excluded from the shaded module
-          This is because that package is not shaded, we want to use these class loaded in user's class loader to do serialization.
-          These classes are used by the Query object below.
-
-          This is not the best way because we can't use the shaded jackson to serialize/deserialize shaded joda objects anymore.
-         */
-        this.om.registerModule(new JodaModule());
-    }
-
     @Override
     public void after(AopContext aopContext) {
         ITraceContext ctx = TraceContextHolder.current();
         if (ctx != null) {
             QueryLifecycle lifecycle = aopContext.getTargetAs();
             if (!ctx.currentSpan().tags().containsKey("query")) {
-                try {
-                    ctx.currentSpan()
-                       .tag("query_id", lifecycle.getQuery().getId())
-                       .tag("query", om.writeValueAsString(lifecycle.getQuery()));
-                } catch (JsonProcessingException e) {
-                    LoggerFactory.getLogger(QueryLifecycle$Initialize.class).error("Unable to serialize query object: {}", e.getMessage());
-                }
+                ctx.currentSpan()
+                   .tag("query_id", lifecycle.getQuery().getId())
+                   .tag(Tags.Database.STATEMENT, lifecycle.getQuery().toString());
             }
         }
     }
