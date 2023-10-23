cd85a90e75753c18b14d48f53f7abb8736626337
frank chen <frank.chen021@outlook.com>
2023-10-23 14:57:09 +0800

    Add index format

--- server/web-app/src/main/resources/static/js/component/table/component.js
+++ server/web-app/src/main/resources/static/js/component/table/component.js
@@ -66,6 +66,7 @@ class TableComponent {
         this.mFormatters['detail'] = (val, row, index) => val !== "" ? `<button class="btn btn-sm btn-outline-info" onclick="toggleTableDetailView('${option.tableId}', ${index})">Toggle</button>` : '';
         this.mFormatters['dialog'] = (val, row, index, field) => val !== "" ? `<button class="btn btn-sm btn-outline-info" onclick="showTableDetailViewInDlg('${option.tableId}', ${index}, '${field}')">Show</button>` : '';
         this.mFormatters['block'] = (val, row, index) => `<pre>${val}</pre>`;
+        this.mFormatters['index'] = (val, row, index) => index + 1;
         this.mFormatters['kv'] = (val, row, index) => {
             let text = '<pre style="margin-bottom: 0">';
             for (const propName in val) {
