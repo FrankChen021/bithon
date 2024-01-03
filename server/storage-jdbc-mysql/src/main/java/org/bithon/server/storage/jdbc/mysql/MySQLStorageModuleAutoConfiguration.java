26d21cec04dbe15e36a845e6cd54882e4dc783d6
frank chen <frank.chen021@outlook.com>
2024-01-04 00:00:02 +0800

    Add MySQL storage to improve the abstract design

---
+++ server/storage-jdbc-mysql/src/main/java/org/bithon/server/storage/jdbc/mysql/MySQLStorageModuleAutoConfiguration.java
@@ -0,0 +1,52 @@
+/*
+ *    Copyright 2020 bithon.org
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.bithon.server.storage.jdbc.mysql;
+
+import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.databind.Module;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author frank.chen021@outlook.com
+ * @date 25/10/21 9:45 pm
+ */
+@Configuration
+@AutoConfigureBefore({DataSourceAutoConfiguration.class})
+public class MySQLStorageModuleAutoConfiguration {
+    @Bean
+    public Module mySQLStorageModel() {
+        return new Module() {
+            @Override
+            public String getModuleName() {
+                return "storage-jdbc-mysql";
+            }
+
+            @Override
+            public Version version() {
+                return Version.unknownVersion();
+            }
+
+            @Override
+            public void setupModule(SetupContext context) {
+                context.registerSubtypes(MySQLSqlDialect.class);
+            }
+        };
+    }
+}
