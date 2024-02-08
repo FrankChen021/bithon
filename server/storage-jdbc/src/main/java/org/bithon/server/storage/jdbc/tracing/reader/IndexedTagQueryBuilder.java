FrankChen021/bithon

commit abe75e28d51ed081a0a95e3d03611c4df75e7430

Uplift data source reader (#716)
* Uplift IMetricReader

* Fix bug to use correct column name

* Allow to close metric reader

* Fix style

* Rename IMetricReader to IDataSourceReader

* Fix style

* Fix UT

* Extract IDataSource from DataSourceSchema

* Extract Trace/Event datasource

* Extract ExternalDataSourceSpec

* Fix trace

* Fix AgentConfigurationApi

* Unify IEventReader implementation

* DataSourceSchema --> Schema

* Update event/trace schema

* Fix dependency

* Fix comments

* Clean the IDataStoreSpec interface

* Update name

server/storage-jdbc/src/main/java/org/bithon/server/storage/jdbc/tracing/reader/IndexedTagQueryBuilder.java
@@ -18,14 +18,13 @@ package org.bithon.server.storage.jdbc.tracing.reader;
 
 import org.bithon.component.commons.expression.IExpression;
 import org.bithon.component.commons.expression.IdentifierExpression;
-import org.bithon.component.commons.utils.StringUtils;
 import org.bithon.server.storage.jdbc.common.dialect.Expression2Sql;
 import org.bithon.server.storage.jdbc.common.dialect.ISqlDialect;
 import org.bithon.server.storage.jdbc.common.jooq.Tables;
 import org.jooq.Record1;
 import org.jooq.SelectConditionStep;
 
-import java.util.Map;
+import java.util.List;
 
 /**
  * @author frank.chen021@outlook.com
@@ -40,23 +39,14 @@ class IndexedTagQueryBuilder extends NestQueryBuilder {
     }
 
     @Override
-    public SelectConditionStep<Record1<String>> build(Map<Integer, IExpression> filters) {
+    public SelectConditionStep<Record1<String>> build(List<IExpression> filters) {
         if (filters.isEmpty()) {
             return null;
         }
 
+        Expression2Sql expression2Sql = new Expression2Sql(null, sqlDialect);
         SelectConditionStep<Record1<String>> query = null;
-        for (Map.Entry<Integer, IExpression> entry : filters.entrySet()) {
-            Integer index = entry.getKey();
-            IExpression filter = entry.getValue();
-
-            if (index > Tables.BITHON_TRACE_SPAN_TAG_INDEX.fieldsRow().size() - 2) {
-                throw new RuntimeException(StringUtils.format("Tag [%s] is configured to use wrong index [%d]. Should be in the range [1, %d]",
-                                                              filter.serializeToText(),
-                                                              index,
-                                                              Tables.BITHON_TRACE_SPAN_TAG_INDEX.fieldsRow().size() - 2));
-            }
-
+        for (IExpression filter : filters) {
             if (query == null) {
                 query = dslContext.select(Tables.BITHON_TRACE_SPAN_TAG_INDEX.TRACEID)
                                   .from(Tables.BITHON_TRACE_SPAN_TAG_INDEX)
@@ -67,7 +57,7 @@ class IndexedTagQueryBuilder extends NestQueryBuilder {
             // NOTE:
             // instantiate the TagFilterSerializer for each loop
             // because it internally holds some states for each 'serialize' method call
-            query = query.and(new TagFilterSerializer(sqlDialect, index).serialize(filter));
+            query = query.and(expression2Sql.serialize(filter));
         }
 
         if (query != null) {

commit 2c3e88dd0ad0f57a2223f029df84162a4549f344

Fix bug caused by #716

server/storage-jdbc/src/main/java/org/bithon/server/storage/jdbc/tracing/reader/IndexedTagQueryBuilder.java
@@ -44,7 +44,6 @@ class IndexedTagQueryBuilder extends NestQueryBuilder {
             return null;
         }
 
-        Expression2Sql expression2Sql = new Expression2Sql(null, sqlDialect);
         SelectConditionStep<Record1<String>> query = null;
         for (IExpression filter : filters) {
             if (query == null) {
@@ -57,7 +56,7 @@ class IndexedTagQueryBuilder extends NestQueryBuilder {
             // NOTE:
             // instantiate the TagFilterSerializer for each loop
             // because it internally holds some states for each 'serialize' method call
-            query = query.and(expression2Sql.serialize(filter));
+            query = query.and(new Expression2Sql(null, sqlDialect).serialize(filter));
         }
 
         if (query != null) {