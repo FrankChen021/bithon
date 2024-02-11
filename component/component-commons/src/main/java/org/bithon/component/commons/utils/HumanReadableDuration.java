85ead5079785ce3e1045f4041d2b52dcafd3e712
Frank Chen <frank.chen021@outlook.com>
2024-02-11 12:08:58 +0800

    Add HumanReadableDuration

---
+++ component/component-commons/src/main/java/org/bithon/component/commons/utils/HumanReadableDuration.java
@@ -0,0 +1,102 @@
+/*
+ *    Copyright 2020 bithon.org
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.bithon.component.commons.utils;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author frank.chen021@outlook.com
+ * @date 2024/2/11 11:38
+ */
+public class HumanReadableDuration {
+    private final String text;
+    private final Duration duration;
+    private final TimeUnit unit;
+
+    public HumanReadableDuration(String durationText,
+                                 Duration duration,
+                                 TimeUnit unit) {
+        this.text = durationText;
+        this.duration = duration;
+        this.unit = unit;
+    }
+
+    public Duration getDuration() {
+        return duration;
+    }
+
+    public TimeUnit getUnit() {
+        return unit;
+    }
+
+    @Override
+    public String toString() {
+        return text;
+    }
+
+    public static HumanReadableDuration parse(String durationText) {
+        Preconditions.checkNotNull(durationText, "durationText can not be null.");
+        durationText = durationText.trim();
+        Preconditions.checkIfTrue(durationText.length() >= 2, "durationText has invalid format.");
+
+        TimeUnit timeUnit;
+        char unit = durationText.charAt(durationText.length() - 1);
+        switch (unit) {
+            case 's':
+            case 'S':
+                timeUnit = TimeUnit.SECONDS;
+                break;
+
+            case 'm':
+            case 'M':
+                timeUnit = TimeUnit.MINUTES;
+                break;
+
+            case 'h':
+            case 'H':
+                timeUnit = TimeUnit.HOURS;
+                break;
+
+            case 'd':
+            case 'D':
+                timeUnit = TimeUnit.DAYS;
+                break;
+
+            default:
+                throw new UnsupportedOperationException(unit + " is not supported");
+        }
+
+        int val = 0;
+        for (int i = 0, len = durationText.length() - 1; i < len; i++) {
+            char chr = durationText.charAt(i);
+            if (!Character.isDigit(chr)) {
+                throw new RuntimeException("Invalid number");
+            }
+
+            int v = val + chr - '0';
+            if (v < val) {
+                throw new RuntimeException("The number is out of range of Integer");
+            }
+            val = v;
+        }
+
+        return new HumanReadableDuration(durationText,
+                                         Duration.ofSeconds(timeUnit.toSeconds(val)),
+                                         timeUnit);
+    }
+}
---
+++ component/component-commons/src/test/java/org/bithon/component/commons/utils/HumanReadableDurationTest.java
@@ -0,0 +1,67 @@
+/*
+ *    Copyright 2020 bithon.org
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.bithon.component.commons.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author frank.chen021@outlook.com
+ * @date 2024/2/11 11:54
+ */
+public class HumanReadableDurationTest {
+    @Test
+    public void testSecond() {
+        HumanReadableDuration duration = HumanReadableDuration.parse("5s");
+        Assert.assertEquals(5, duration.getDuration().getSeconds());
+        Assert.assertEquals(TimeUnit.SECONDS, duration.getUnit());
+        Assert.assertEquals("5s", duration.toString());
+    }
+
+    @Test
+    public void testMinute() {
+        HumanReadableDuration duration = HumanReadableDuration.parse("5m");
+        Assert.assertEquals(5, duration.getDuration().toMinutes());
+        Assert.assertEquals(TimeUnit.MINUTES, duration.getUnit());
+        Assert.assertEquals("5m", duration.toString());
+    }
+
+    @Test
+    public void testHour() {
+        HumanReadableDuration duration = HumanReadableDuration.parse("7h");
+        Assert.assertEquals(7, duration.getDuration().toHours());
+        Assert.assertEquals(TimeUnit.HOURS, duration.getUnit());
+        Assert.assertEquals("7h", duration.toString());
+    }
+
+    @Test
+    public void testDay() {
+        HumanReadableDuration duration = HumanReadableDuration.parse("7d");
+        Assert.assertEquals(7, duration.getDuration().toDays());
+        Assert.assertEquals(TimeUnit.DAYS, duration.getUnit());
+        Assert.assertEquals("7d", duration.toString());
+    }
+
+    @Test
+    public void testInvalidFormat() {
+        Assert.assertThrows(RuntimeException.class, () -> HumanReadableDuration.parse("6"));
+        Assert.assertThrows(RuntimeException.class, () -> HumanReadableDuration.parse("7a"));
+        Assert.assertThrows(RuntimeException.class, () -> HumanReadableDuration.parse("dd"));
+    }
+}
