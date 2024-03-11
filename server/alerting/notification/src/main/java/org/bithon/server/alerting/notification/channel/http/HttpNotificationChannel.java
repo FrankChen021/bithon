813718714e39604a74bf5a1b7bdc2656e866a65f
frank chen <frank.chen021@outlook.com>
2024-03-11 13:42:13 +0800

    Add new contract

--- server/alerting/notification/src/main/java/org/bithon/server/alerting/notification/channel/http/HttpNotificationChannel.java
+++ server/alerting/notification/src/main/java/org/bithon/server/alerting/notification/channel/http/HttpNotificationChannel.java
@@ -39,13 +39,15 @@ import org.bithon.server.alerting.notification.channel.INotificationChannel;
 import org.bithon.server.alerting.notification.message.NotificationMessage;
 import org.springframework.http.MediaType;
 
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.Map;
 
 /**
+ * Notification via HTTP POST
+ *
  * @author Frank Chen
  * @date 18/3/22 10:09 PM
  */
@@ -57,10 +59,31 @@ public class HttpNotificationChannel implements INotificationChannel {
 
     @Data
     public static class Props {
-        @NotEmpty
+        @NotBlank
         private String url;
 
+        /**
+         * Extra headers.
+         * Can be NULL or empty
+         */
         private Map<String, String> headers;
+
+        /**
+         * Explicitly defined header so that user must provide such configuration
+         */
+        @NotBlank
+        private String contentType;
+
+        /**
+         * The body template.
+         * Supported variables (must be braced in brackets)
+         *  alert.appName: The application name of the alert belonged to
+         *  alert.name: The name of the alert
+         *  alert.expr: The expression that the alert runs on
+         *  alert.url: The url that users can view the detail of this alert record
+         */
+        @NotBlank
+        private String body;
     }
 
     @Getter
@@ -80,17 +103,22 @@ public class HttpNotificationChannel implements INotificationChannel {
 
     @Override
     public void send(NotificationMessage message) throws IOException {
-        HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
-        try (CloseableHttpClient client = httpClientBuilder.build()) {
+        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
             HttpPost request = new HttpPost(this.props.url);
-            request.setEntity(new StringEntity(objectMapper.writeValueAsString(message), StandardCharsets.UTF_8));
-            request.setHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE);
+            // Mandatory header
+            request.setHeader("Content-Type", props.contentType);
+
+            // Custom headers
             request.setHeaders(this.props
                                    .headers
                                    .keySet()
                                    .stream()
                                    .map(k -> new BasicHeader(k, this.props.headers.get(k)))
                                    .toArray(Header[]::new));
+
+            // Body
+            request.setEntity(new StringEntity(objectMapper.writeValueAsString(message), StandardCharsets.UTF_8));
+
             HttpResponse response = client.execute(request);
             if (response.getStatusLine().getStatusCode() >= 400) {
                 throw new RuntimeException(StringUtils.format("Failed to send message to [%s]: Received status: %d, response: %s",
