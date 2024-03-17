FrankChen021/bithon

commit b07c54cb34acd8a94909b7f80f67e81e3747ac95

Rework transformer (#754)
* Rename 'As' to 'Duplicate'

* Add comments

* Add replace/expression transformer

* Rename Filter to Drop and invert the condition internally

* Rename duplicate to flatten

* Deprecate legacy transformers

* Introduce AbstractTransformer

* Deprecate IInputRowFilter and IFlattener

* Update AbstractTransformer visibility

* Add test

* Relocate transform package

* Fix UT

server/pipeline/src/main/java/org/bithon/server/pipeline/common/transform/transformer/AbstractTransformer.java
@@ -1,0 +1,56 @@
+/*
+ *    Copyright 2020 bithon.org
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.bithon.server.pipeline.common.transform.transformer;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import org.bithon.component.commons.expression.ConditionalExpression;
+import org.bithon.component.commons.expression.IExpression;
+import org.bithon.component.commons.expression.LogicalExpression;
+import org.bithon.component.commons.utils.Preconditions;
+import org.bithon.server.storage.common.expression.ExpressionASTBuilder;
+import org.bithon.server.storage.datasource.builtin.Functions;
+import org.bithon.server.storage.datasource.input.IInputRow;
+
+public abstract class AbstractTransformer implements ITransformer {
+
+    @JsonIgnore
+    private final IExpression whereCondition;
+
+    @Getter
+    private final String where;
+
+    protected AbstractTransformer(String where) {
+        this.where = where;
+
+        this.whereCondition = this.where == null ? null : ExpressionASTBuilder.builder().functions(Functions.getInstance()).build(this.where);
+        Preconditions.checkIfTrue(this.whereCondition == null || this.whereCondition instanceof LogicalExpression || this.whereCondition instanceof ConditionalExpression,
+                                  "The 'where' property must be a LOGICAL or CONDITIONAL expression");
+    }
+
+    public TransformResult transform(IInputRow inputRow) throws TransformException {
+        if (whereCondition != null) {
+            boolean satisfied = (boolean) whereCondition.evaluate(inputRow);
+            if (!satisfied) {
+                return TransformResult.CONTINUE;
+            }
+        }
+        return transformInternal(inputRow);
+    }
+
+    protected abstract TransformResult transformInternal(IInputRow inputRow) throws TransformException;
+}

server/pipeline/src/main/java/org/bithon/server/pipeline/common/transform/transformer/DropTransformer.java
@@ -1,0 +1,64 @@
+/*
+ *    Copyright 2020 bithon.org
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.bithon.server.pipeline.common.transform.transformer;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import org.bithon.component.commons.expression.ConditionalExpression;
+import org.bithon.component.commons.expression.IExpression;
+import org.bithon.component.commons.expression.LogicalExpression;
+import org.bithon.component.commons.utils.Preconditions;
+import org.bithon.server.storage.common.expression.ExpressionASTBuilder;
+import org.bithon.server.storage.datasource.builtin.Functions;
+import org.bithon.server.storage.datasource.input.IInputRow;
+
+/**
+ * @author Frank Chen
+ * @date 22/1/24 10:31 pm
+ */
+public class DropTransformer extends AbstractTransformer {
+    @Getter
+    private final String expr;
+
+    @Getter
+    private final boolean debug;
+
+    @JsonIgnore
+    private final IExpression dropCondition;
+
+    @JsonCreator
+    public DropTransformer(@JsonProperty("expr") String expr,
+                           @JsonProperty("debug") Boolean debug,
+                           @JsonProperty("where") String where) {
+        super(where);
+
+        this.expr = expr;
+        this.debug = debug != null && debug;
+
+        this.dropCondition = ExpressionASTBuilder.builder().functions(Functions.getInstance()).build(this.expr);
+        Preconditions.checkIfTrue(this.dropCondition instanceof LogicalExpression || this.dropCondition instanceof ConditionalExpression,
+                                  "The 'expr' property must be a LOGICAL or CONDITIONAL expression");
+    }
+
+    @Override
+    protected TransformResult transformInternal(IInputRow inputRow) throws TransformException {
+        // When the expression satisfies, this input should be DROPPED
+        return ((boolean) dropCondition.evaluate(inputRow)) ? TransformResult.DROP : TransformResult.CONTINUE;
+    }
+}

commit 66047c32f3d47755a19d4824944f18d00da96b5b

Fix bugs introduced by #754

server/pipeline/src/main/java/org/bithon/server/pipeline/common/transform/transformer/AbstractTransformer.java
@@ -19,6 +19,8 @@ package org.bithon.server.pipeline.common.transform.transformer;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import org.bithon.component.commons.expression.ConditionalExpression;
+import org.bithon.component.commons.expression.FunctionExpression;
+import org.bithon.component.commons.expression.IDataType;
 import org.bithon.component.commons.expression.IExpression;
 import org.bithon.component.commons.expression.LogicalExpression;
 import org.bithon.component.commons.utils.Preconditions;
@@ -38,8 +40,9 @@ public abstract class AbstractTransformer implements ITransformer {
         this.where = where;
 
         this.whereCondition = this.where == null ? null : ExpressionASTBuilder.builder().functions(Functions.getInstance()).build(this.where);
-        Preconditions.checkIfTrue(this.whereCondition == null || this.whereCondition instanceof LogicalExpression || this.whereCondition instanceof ConditionalExpression,
-                                  "The 'where' property must be a LOGICAL or CONDITIONAL expression");
+        if (this.whereCondition != null) {
+            validateConditionExpression(whereCondition);
+        }
     }
 
     public TransformResult transform(IInputRow inputRow) throws TransformException {
@@ -53,4 +56,12 @@ public abstract class AbstractTransformer implements ITransformer {
     }
 
     protected abstract TransformResult transformInternal(IInputRow inputRow) throws TransformException;
+
+    static void validateConditionExpression(IExpression expression) {
+        Preconditions.checkIfTrue(expression instanceof LogicalExpression
+                                  || expression instanceof ConditionalExpression
+                                  || (expression instanceof FunctionExpression && expression.getDataType().equals(IDataType.BOOLEAN)),
+                                  "The expression [%s] must be a LOGICAL/CONDITIONAL or a function expression that returns boolean",
+                                  expression.serializeToText());
+    }
 }

server/pipeline/src/main/java/org/bithon/server/pipeline/common/transform/transformer/DropTransformer.java
@@ -20,9 +20,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
-import org.bithon.component.commons.expression.ConditionalExpression;
 import org.bithon.component.commons.expression.IExpression;
-import org.bithon.component.commons.expression.LogicalExpression;
 import org.bithon.component.commons.utils.Preconditions;
 import org.bithon.server.storage.common.expression.ExpressionASTBuilder;
 import org.bithon.server.storage.datasource.builtin.Functions;
@@ -40,7 +38,7 @@ public class DropTransformer extends AbstractTransformer {
     private final boolean debug;
 
     @JsonIgnore
-    private final IExpression dropCondition;
+    private final IExpression dropExpression;
 
     @JsonCreator
     public DropTransformer(@JsonProperty("expr") String expr,
@@ -48,17 +46,17 @@ public class DropTransformer extends AbstractTransformer {
                            @JsonProperty("where") String where) {
         super(where);
 
-        this.expr = expr;
+        this.expr = Preconditions.checkArgumentNotNull("expr", expr);
         this.debug = debug != null && debug;
 
-        this.dropCondition = ExpressionASTBuilder.builder().functions(Functions.getInstance()).build(this.expr);
-        Preconditions.checkIfTrue(this.dropCondition instanceof LogicalExpression || this.dropCondition instanceof ConditionalExpression,
-                                  "The 'expr' property must be a LOGICAL or CONDITIONAL expression");
+        this.dropExpression = ExpressionASTBuilder.builder()
+                                                  .functions(Functions.getInstance()).build(this.expr);
+        AbstractTransformer.validateConditionExpression(this.dropExpression);
     }
 
     @Override
     protected TransformResult transformInternal(IInputRow inputRow) throws TransformException {
         // When the expression satisfies, this input should be DROPPED
-        return ((boolean) dropCondition.evaluate(inputRow)) ? TransformResult.DROP : TransformResult.CONTINUE;
+        return ((boolean) dropExpression.evaluate(inputRow)) ? TransformResult.DROP : TransformResult.CONTINUE;
     }
 }

server/server-starter/src/main/resources/application-pipeline-from-kafka.yml
@@ -43,8 +43,8 @@ bithon:
             "[bootstrap.servers]": localhost:9092
             "[fetch.min.bytes]": 1048576
       processors:
-        - type: filter
-          expr: not endsWith(clazz,  'UriNormalizer')
+        - type: drop
+          expr: endsWith(clazz,  'UriNormalizer')
         - type: url-sanitize-transform
           sensitiveParameters:
             "[http.uri]": password

server/server-starter/src/main/resources/application-pipeline-to-kafka-to-local.yml
@@ -105,8 +105,8 @@ bithon:
             "[bootstrap.servers]": localhost:9092
             "[fetch.min.bytes]": 1048576
       processors:
-        - type: filter
-          expr: not endsWith(clazz,  'UriNormalizer')
+        - type: drop
+          expr: endsWith(clazz,  'UriNormalizer')
         - type: url-sanitize-transform
           sensitiveParameters:
             "[http.uri]": password

server/server-starter/src/main/resources/application-pipeline-to-local.yml
@@ -22,8 +22,8 @@ bithon:
         - type: otlp-trace-grpc
         - type: otlp-trace-http
       processors:
-        - type: filter
-          expr: not endsWith(clazz,  'UriNormalizer')
+        - type: drop
+          expr: endsWith(clazz,  'UriNormalizer')
         - type: url-sanitize-transform
           sensitiveParameters:
             "[http.uri]": password