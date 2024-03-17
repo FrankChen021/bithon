FrankChen021/bithon

commit b07c54cb34acd8a94909b7f80f67e81e3747ac95

Rework transformer (#754)
* Rename 'As' to 'Duplicate'

* Add comments

* Add replace/expression transformer

* Rename Filter to Drop and invert the condition internally

* Rename duplicate to flatten

* Deprecate legacy transformers

* Introduce AbstractTransformer

* Deprecate IInputRowFilter and IFlattener

* Update AbstractTransformer visibility

* Add test

* Relocate transform package

* Fix UT

server/pipeline/src/main/java/org/bithon/server/pipeline/common/transform/transformer/AbstractTransformer.java
@@ -1,0 +1,56 @@
+/*
+ *    Copyright 2020 bithon.org
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.bithon.server.pipeline.common.transform.transformer;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import org.bithon.component.commons.expression.ConditionalExpression;
+import org.bithon.component.commons.expression.IExpression;
+import org.bithon.component.commons.expression.LogicalExpression;
+import org.bithon.component.commons.utils.Preconditions;
+import org.bithon.server.storage.common.expression.ExpressionASTBuilder;
+import org.bithon.server.storage.datasource.builtin.Functions;
+import org.bithon.server.storage.datasource.input.IInputRow;
+
+public abstract class AbstractTransformer implements ITransformer {
+
+    @JsonIgnore
+    private final IExpression whereCondition;
+
+    @Getter
+    private final String where;
+
+    protected AbstractTransformer(String where) {
+        this.where = where;
+
+        this.whereCondition = this.where == null ? null : ExpressionASTBuilder.builder().functions(Functions.getInstance()).build(this.where);
+        Preconditions.checkIfTrue(this.whereCondition == null || this.whereCondition instanceof LogicalExpression || this.whereCondition instanceof ConditionalExpression,
+                                  "The 'where' property must be a LOGICAL or CONDITIONAL expression");
+    }
+
+    public TransformResult transform(IInputRow inputRow) throws TransformException {
+        if (whereCondition != null) {
+            boolean satisfied = (boolean) whereCondition.evaluate(inputRow);
+            if (!satisfied) {
+                return TransformResult.CONTINUE;
+            }
+        }
+        return transformInternal(inputRow);
+    }
+
+    protected abstract TransformResult transformInternal(IInputRow inputRow) throws TransformException;
+}

server/pipeline/src/main/java/org/bithon/server/pipeline/common/transform/transformer/DropTransformer.java
@@ -1,0 +1,64 @@
+/*
+ *    Copyright 2020 bithon.org
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.bithon.server.pipeline.common.transform.transformer;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import org.bithon.component.commons.expression.ConditionalExpression;
+import org.bithon.component.commons.expression.IExpression;
+import org.bithon.component.commons.expression.LogicalExpression;
+import org.bithon.component.commons.utils.Preconditions;
+import org.bithon.server.storage.common.expression.ExpressionASTBuilder;
+import org.bithon.server.storage.datasource.builtin.Functions;
+import org.bithon.server.storage.datasource.input.IInputRow;
+
+/**
+ * @author Frank Chen
+ * @date 22/1/24 10:31 pm
+ */
+public class DropTransformer extends AbstractTransformer {
+    @Getter
+    private final String expr;
+
+    @Getter
+    private final boolean debug;
+
+    @JsonIgnore
+    private final IExpression dropCondition;
+
+    @JsonCreator
+    public DropTransformer(@JsonProperty("expr") String expr,
+                           @JsonProperty("debug") Boolean debug,
+                           @JsonProperty("where") String where) {
+        super(where);
+
+        this.expr = expr;
+        this.debug = debug != null && debug;
+
+        this.dropCondition = ExpressionASTBuilder.builder().functions(Functions.getInstance()).build(this.expr);
+        Preconditions.checkIfTrue(this.dropCondition instanceof LogicalExpression || this.dropCondition instanceof ConditionalExpression,
+                                  "The 'expr' property must be a LOGICAL or CONDITIONAL expression");
+    }
+
+    @Override
+    protected TransformResult transformInternal(IInputRow inputRow) throws TransformException {
+        // When the expression satisfies, this input should be DROPPED
+        return ((boolean) dropCondition.evaluate(inputRow)) ? TransformResult.DROP : TransformResult.CONTINUE;
+    }
+}

commit 4a4c080b3e781e3a935bf2076ace48bf488ed184

Update event page to use configuration-based page (#757)
* Fix bugs introduced by #754

* Use dashboard to replace legacy event page to leverage existing sorting

server/pipeline/src/main/java/org/bithon/server/pipeline/common/transform/transformer/AbstractTransformer.java
@@ -19,6 +19,8 @@ package org.bithon.server.pipeline.common.transform.transformer;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import org.bithon.component.commons.expression.ConditionalExpression;
+import org.bithon.component.commons.expression.FunctionExpression;
+import org.bithon.component.commons.expression.IDataType;
 import org.bithon.component.commons.expression.IExpression;
 import org.bithon.component.commons.expression.LogicalExpression;
 import org.bithon.component.commons.utils.Preconditions;
@@ -38,8 +40,9 @@ public abstract class AbstractTransformer implements ITransformer {
         this.where = where;
 
         this.whereCondition = this.where == null ? null : ExpressionASTBuilder.builder().functions(Functions.getInstance()).build(this.where);
-        Preconditions.checkIfTrue(this.whereCondition == null || this.whereCondition instanceof LogicalExpression || this.whereCondition instanceof ConditionalExpression,
-                                  "The 'where' property must be a LOGICAL or CONDITIONAL expression");
+        if (this.whereCondition != null) {
+            validateConditionExpression(whereCondition);
+        }
     }
 
     public TransformResult transform(IInputRow inputRow) throws TransformException {
@@ -53,4 +56,12 @@ public abstract class AbstractTransformer implements ITransformer {
     }
 
     protected abstract TransformResult transformInternal(IInputRow inputRow) throws TransformException;
+
+    static void validateConditionExpression(IExpression expression) {
+        Preconditions.checkIfTrue(expression instanceof LogicalExpression
+                                  || expression instanceof ConditionalExpression
+                                  || (expression instanceof FunctionExpression && expression.getDataType().equals(IDataType.BOOLEAN)),
+                                  "The expression [%s] must be a LOGICAL/CONDITIONAL or a function expression that returns boolean",
+                                  expression.serializeToText());
+    }
 }

server/pipeline/src/main/java/org/bithon/server/pipeline/common/transform/transformer/DropTransformer.java
@@ -20,9 +20,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
-import org.bithon.component.commons.expression.ConditionalExpression;
 import org.bithon.component.commons.expression.IExpression;
-import org.bithon.component.commons.expression.LogicalExpression;
 import org.bithon.component.commons.utils.Preconditions;
 import org.bithon.server.storage.common.expression.ExpressionASTBuilder;
 import org.bithon.server.storage.datasource.builtin.Functions;
@@ -40,7 +38,7 @@ public class DropTransformer extends AbstractTransformer {
     private final boolean debug;
 
     @JsonIgnore
-    private final IExpression dropCondition;
+    private final IExpression dropExpression;
 
     @JsonCreator
     public DropTransformer(@JsonProperty("expr") String expr,
@@ -48,17 +46,17 @@ public class DropTransformer extends AbstractTransformer {
                            @JsonProperty("where") String where) {
         super(where);
 
-        this.expr = expr;
+        this.expr = Preconditions.checkArgumentNotNull("expr", expr);
         this.debug = debug != null && debug;
 
-        this.dropCondition = ExpressionASTBuilder.builder().functions(Functions.getInstance()).build(this.expr);
-        Preconditions.checkIfTrue(this.dropCondition instanceof LogicalExpression || this.dropCondition instanceof ConditionalExpression,
-                                  "The 'expr' property must be a LOGICAL or CONDITIONAL expression");
+        this.dropExpression = ExpressionASTBuilder.builder()
+                                                  .functions(Functions.getInstance()).build(this.expr);
+        AbstractTransformer.validateConditionExpression(this.dropExpression);
     }
 
     @Override
     protected TransformResult transformInternal(IInputRow inputRow) throws TransformException {
         // When the expression satisfies, this input should be DROPPED
-        return ((boolean) dropCondition.evaluate(inputRow)) ? TransformResult.DROP : TransformResult.CONTINUE;
+        return ((boolean) dropExpression.evaluate(inputRow)) ? TransformResult.DROP : TransformResult.CONTINUE;
     }
 }

server/server-starter/src/main/resources/application-pipeline-from-kafka.yml
@@ -43,8 +43,8 @@ bithon:
             "[bootstrap.servers]": localhost:9092
             "[fetch.min.bytes]": 1048576
       processors:
-        - type: filter
-          expr: not endsWith(clazz,  'UriNormalizer')
+        - type: drop
+          expr: endsWith(clazz,  'UriNormalizer')
         - type: url-sanitize-transform
           sensitiveParameters:
             "[http.uri]": password

server/server-starter/src/main/resources/application-pipeline-to-kafka-to-local.yml
@@ -105,8 +105,8 @@ bithon:
             "[bootstrap.servers]": localhost:9092
             "[fetch.min.bytes]": 1048576
       processors:
-        - type: filter
-          expr: not endsWith(clazz,  'UriNormalizer')
+        - type: drop
+          expr: endsWith(clazz,  'UriNormalizer')
         - type: url-sanitize-transform
           sensitiveParameters:
             "[http.uri]": password

server/server-starter/src/main/resources/application-pipeline-to-local.yml
@@ -22,8 +22,8 @@ bithon:
         - type: otlp-trace-grpc
         - type: otlp-trace-http
       processors:
-        - type: filter
-          expr: not endsWith(clazz,  'UriNormalizer')
+        - type: drop
+          expr: endsWith(clazz,  'UriNormalizer')
         - type: url-sanitize-transform
           sensitiveParameters:
             "[http.uri]": password

server/storage/src/main/java/org/bithon/server/storage/event/EventTableSchema.java
@@ -21,6 +21,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import org.bithon.server.commons.time.Period;
 import org.bithon.server.storage.datasource.ISchema;
 import org.bithon.server.storage.datasource.TimestampSpec;
+import org.bithon.server.storage.datasource.column.DateTimeColumn;
 import org.bithon.server.storage.datasource.column.IColumn;
 import org.bithon.server.storage.datasource.column.StringColumn;
 import org.bithon.server.storage.datasource.column.aggregatable.count.AggregateCountColumn;
@@ -42,12 +43,11 @@ public class EventTableSchema implements ISchema {
     public static ISchema createEventTableSchema(IEventStorage eventStorage) {
         return new EventTableSchema("event",
                                     eventStorage,
-                                    Arrays.asList(new StringColumn("appName",
-                                                                   "appName"),
-                                                  new StringColumn("instanceName",
-                                                                   "instanceName"),
-                                                  new StringColumn("type",
-                                                                   "type"),
+                                    Arrays.asList(new DateTimeColumn("timestamp", "timestamp"),
+                                                  new StringColumn("appName", "appName"),
+                                                  new StringColumn("instanceName", "instanceName"),
+                                                  new StringColumn("type", "type"),
+                                                  new StringColumn("arguments", "arguments"),
                                                   AggregateCountColumn.INSTANCE));
     }
 

server/web-app/src/main/resources/static/js/component/table.js
@@ -68,6 +68,7 @@ class TableComponent {
 
         this.mFormatters = {};
         this.mFormatters['shortDateTime'] = (v) => new Date(v).format('MM-dd hh:mm:ss');
+        this.mFormatters['json_string'] = (val, row, index) => `<button class="btn btn-sm btn-outline-info" onclick="toggleTableDetailView('${option.tableId}', ${index})">Toggle</button>`;
         this.mFormatters['detail'] = (val, row, index) => val !== "" ? `<button class="btn btn-sm btn-outline-info" onclick="toggleTableDetailView('${option.tableId}', ${index})">Toggle</button>` : '';
         this.mFormatters['dialog'] = (val, row, index, field) => val !== "" ? `<button class="btn btn-sm btn-outline-info" onclick="showTableDetailViewInDlg('${option.tableId}', ${index}, '${field}')">Show</button>` : '';
         this.mFormatters['block'] = (val) => `<pre>${val.htmlEncode()}</pre>`;
@@ -129,7 +130,7 @@ class TableComponent {
             if (column.format !== undefined && column.formatter == null) {
                 // formatter is an option provided by bootstrap-table
                 column.formatter = this.mFormatters[column.format];
-                if (column.format === 'detail') {
+                if (column.format === 'detail' || column.format === 'json_string') {
                     this.mDetailViewField = column.field;
                 }
             }
@@ -414,7 +415,12 @@ class TableComponent {
     }
 
     #showDetail(index, row) {
-        return '<pre>' + row[this.mDetailViewField] + '</pre>';
+        let cell = row[this.mDetailViewField];
+        const column = this.mColumnMap[this.mDetailViewField];
+        if (column.format === 'json_string') {
+            cell = JSON.stringify(JSON.parse(cell), null, 2);
+        }
+        return '<pre>' + cell + '</pre>';
     }
 
     #getQueryParams(params) {

server/web-app/src/main/resources/static/js/page/event/page.js
@@ -1,132 +1,0 @@
-class EventPage {
-    constructor(appName) {
-        // Model
-        this.mApplication = appName;
-        this.mInterval = null;
-
-        // View
-        this.vFilter = new AppSelector({
-            appName: appName,
-            parentId: 'filterBar',
-            intervalProvider: () => this.#getInterval(),
-        }).registerChangedListener((name, value) => {
-            if (name === 'application') {
-                g_SelectedApp = value;
-                window.history.pushState('', '', `/web/app/event/${value}`);
-            }
-
-            this.#refreshPage();
-        }).createAppSelector(appName)
-            .createFilterByFields('event', ['instanceName', 'type']);
-
-        const parent = $('#filterBarForm');
-
-        // View - Refresh Button
-        parent.append('<button class="btn btn-outline-secondary" style="border-radius:0;border-color: #ced4da" type="button"><i class="fas fa-sync-alt"></i></button>')
-            .find("button").click(() => {
-            this.mInterval = this.vIntervalSelector.getInterval();
-            this.#refreshPage();
-        });
-
-        // View
-        this.vIntervalSelector = new TimeSpanSelector('all', true).childOf(parent).registerIntervalChangedListener((selectedModel) => {
-            this.mInterval = this.vIntervalSelector.getInterval();
-            this.#refreshPage();
-        });
-        this.mInterval = this.vIntervalSelector.getInterval();
-
-        // View, will also trigger refresh automatically
-        $('#table').bootstrapTable({
-            toolbar: '#toolbar',//工具栏
-
-            url: apiHost + '/api/event/getEventList',
-            method: 'post',
-            contentType: "application/json",
-            showRefresh: false,
-
-            buttonsAlign: 'right',
-            sidePagination: "server",
-            pagination: true,
-            paginationPreText: '<',
-            paginationNextText: '>',
-            pageNumber: 1,
-            pageSize: 10,
-            pageList: [10, 25, 50, 100],
-
-            queryParamsType: '',
-            queryParams: (params) => {
-                const interval = this.#getInterval();
-                return {
-                    pageSize: params.pageSize,
-                    pageNumber: params.pageNumber - 1,
-                    traceId: params.searchText,
-                    filters: this.vFilter.getSelectedFilters(),
-                    startTimeISO8601: interval.start,
-                    endTimeISO8601: interval.end,
-                    orderBy: params.sortName,
-                    order: params.sortOrder
-                };
-            },
-
-            filterControl: false,
-            filterShowClear: false,
-            search: false,
-            showSearchClearButton: false,
-            searchOnEnterKey: true,
-            formatSearch: function () {
-                return '';
-            },
-
-            uniqueId: 'traceId',
-            columns: [{
-                field: 'timestamp',
-                title: 'Time',
-                formatter: function (value) {
-                    return new Date(value).format('yyyy-MM-dd hh:mm:ss');
-                },
-                width: 200
-            }, {
-                field: 'appName',
-                title: 'Application',
-                width: 200
-            }, {
-                field: 'instanceName',
-                title: 'Instance',
-                width: 200
-            }, {
-                field: 'type',
-                title: 'Event',
-                width: 100
-            }, {
-                field: 'arguments',
-                title: 'Args',
-                formatter: function (value, row, index) {
-                    return `<button class="btn btn-sm btn-outline-info" onclick="javascript:toggleArgument(${index})">Toggle argument</button>`;
-                }
-            }],
-
-            detailView: true,
-            detailFormatter: (index, row) => {
-                if (row.formattedArgs === undefined) {
-                    row.formattedArgs = "<pre>" + JSON.stringify(JSON.parse(row.arguments), null, 2) + "</pre>";
-                }
-                return row.formattedArgs;
-            }
-        });
-    }
-
-    #getInterval() {
-        return this.mInterval;//this.mInterval != null ? this.mInterval : this.vIntervalSelector.getInterval();
-    }
-
-    #refreshPage() {
-        //
-        // refresh the list
-        //
-        $('#table').bootstrapTable('refresh');
-    }
-}
-
-function toggleArgument(index) {
-    $('#table').bootstrapTable('toggleDetailView', index);
-}

server/web-app/src/main/resources/templates/app/event.html
@@ -11,12 +11,89 @@
 </head>
 <body>
 
-<div layout:fragment="content">
-
-    <table id="table"></table>
+<div layout:fragment="content" id="dashboard">
 
     <script>
-        new EventPage(g_SelectedApp);
+        const eventPage = {
+                "name": "event",
+                "title": "Events",
+                "filter": {
+                    "selectors": [
+                        {
+                            "type": "datasource",
+                            "name": "event",
+                            "fields": [
+                                {"name": "appName", "width": 200, "allowClear": false},
+                                "instanceName",
+                                "type"
+                            ]
+                        }
+                    ],
+                    "showFilterInput": false
+                },
+                "charts": [
+                    {
+                        "title": "Events List",
+                        "width": 4,
+                        "type": "table",
+                        "showColumns": false,
+                        "columns": [
+                            {
+                                "name": "timestamp",
+                                "format": "time",
+                                "template": "MM-dd hh:mm:ss.S",
+                                "width": 200
+                            },
+                            "instanceName",
+                            "type",
+                            {
+                                "name": "arguments",
+                                "format": "json_string",
+                                "sortable": false
+                            }
+                        ],
+                        "pagination": [
+                            50,
+                            100,
+                            500
+                        ],
+                        "query": {
+                            "dataSource": "event",
+                            "type": "list",
+                            "fields": [
+                                "timestamp",
+                                "instanceName",
+                                "type",
+                                "arguments",
+                            ],
+                            "orderBy": {
+                                "name": "timestamp",
+                                "order": "desc"
+                            },
+                            "precondition": {
+                                "filters": [
+                                    "appName"
+                                ]
+                            }
+                        }
+                    }
+                ]
+            };
+
+        $(document).ready(() => {
+
+            // Initialize dashboard
+            const dashboard = new Dashboard('dashboard',
+                'Event',
+                window.queryParams['interval'],
+                false);
+
+            dashboard.load(eventPage);
+
+            $(window).bind('resize', () => {
+                dashboard.resize();
+            });
+        });
     </script>
 </div>
 </body>