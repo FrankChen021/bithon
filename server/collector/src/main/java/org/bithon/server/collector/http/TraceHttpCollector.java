1f0949fc125a7e353990ba8fc849d9d8dfa3cbec
frank chen <frank.chen021@outlook.com>
2024-03-31 18:19:47 +0800

    Limit the size of logs deserialized

--- server/collector/src/main/java/org/bithon/server/collector/http/TraceHttpCollector.java
+++ server/collector/src/main/java/org/bithon/server/collector/http/TraceHttpCollector.java
@@ -18,11 +18,12 @@ package org.bithon.server.collector.http;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.core.json.UTF8StreamJsonParser;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.bithon.component.commons.concurrency.NamedThreadFactory;
 import org.bithon.component.commons.utils.ReflectionUtils;
 import org.bithon.component.commons.utils.StringUtils;
 import org.bithon.server.pipeline.tracing.ITraceProcessor;
@@ -39,6 +40,9 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.InflaterInputStream;
 
@@ -52,12 +56,23 @@ import java.util.zip.InflaterInputStream;
 public class TraceHttpCollector {
 
     private final ObjectMapper om;
+    private final TraceHttpCollectorConfig config;
+    private final ThreadPoolExecutor executor;
 
     @Setter
     private ITraceProcessor processor;
 
-    public TraceHttpCollector(ObjectMapper om) {
-        this.om = om;
+    public TraceHttpCollector(TraceHttpCollectorConfig config,
+                              ObjectMapper objectMapper) {
+        this.config = config;
+        this.om = objectMapper;
+        this.executor = new ThreadPoolExecutor(1,
+                                               4,
+                                               1L,
+                                               TimeUnit.MINUTES,
+                                               new LinkedBlockingQueue<>(8),
+                                               NamedThreadFactory.of("trace-http-processor"),
+                                               new ThreadPoolExecutor.CallerRunsPolicy());
     }
 
     @PostMapping("/api/collector/trace")
@@ -82,13 +97,29 @@ public class TraceHttpCollector {
             }
         }
 
-        List<TraceSpan> spans;
+        List<TraceSpan> spans = new ArrayList<>(256);
 
-        // create parser manually so that this parser can be accessed in the catch handler
+        // Create parser manually so that this parser can be accessed in the catch handler
         try (JsonParser parser = om.createParser(is)) {
             try {
-                spans = om.readValue(parser, new TypeReference<ArrayList<TraceSpan>>() {
-                });
+                JsonToken token = parser.nextToken();
+                if (token != JsonToken.START_ARRAY) {
+                    return;
+                }
+
+                // JSONArray format
+                while (parser.nextToken() == JsonToken.START_OBJECT) {
+                    spans.add(om.readValue(parser, TraceSpan.class));
+
+                    // This controls the size of content reading from HTTP,
+                    // to avoid excessive memory pressure at the collector side
+                    if (spans.size() >= config.getMaxRowsPerBatch()) {
+                        process(spans);
+
+                        // Create a new array to hold the next batch
+                        spans = new ArrayList<>(256);
+                    }
+                }
             } catch (IOException e) {
                 String message = StringUtils.format("Invalid input from [%s]: %s", request.getRemoteAddr(), e.getMessage());
                 log.error(message, e);
@@ -112,10 +143,14 @@ public class TraceHttpCollector {
                 return;
             }
         }
+
+        process(spans);
+    }
+
+    private void process(List<TraceSpan> spans) {
         if (spans.isEmpty()) {
             return;
         }
-
-        this.processor.process("trace", spans);
+        this.executor.execute(() -> this.processor.process("trace", spans));
     }
 }
