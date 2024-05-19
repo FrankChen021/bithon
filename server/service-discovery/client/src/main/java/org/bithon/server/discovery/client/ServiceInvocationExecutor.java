fb4eb4ec876f786367a7b478711ef2858b9f04ff
frank chen <frank.chen021@outlook.com>
2024-05-19 10:09:45 +0800

    Fix CI

--- server/service-discovery/client/src/main/java/org/bithon/server/discovery/client/ServiceInvocationExecutor.java
+++ server/service-discovery/client/src/main/java/org/bithon/server/discovery/client/ServiceInvocationExecutor.java
@@ -23,7 +23,6 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 /**
