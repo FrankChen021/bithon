4c2c661c0579fdc112ab0684454d403eafb16f8b
frank chen <frank.chen021@outlook.com>
2024-07-06 23:26:44 +0800

    Support kafka consumer 3.7

--- agent/agent-plugins/apache-kafka/src/main/java/org/bithon/agent/plugin/apache/kafka/consumer/interceptor/KafkaConsumer$Ctor.java
+++ agent/agent-plugins/apache-kafka/src/main/java/org/bithon/agent/plugin/apache/kafka/consumer/interceptor/KafkaConsumer$Ctor.java
@@ -61,9 +61,36 @@ public class KafkaConsumer$Ctor extends AfterInterceptor {
         kafkaConsumer.setInjectedObject(kafkaPluginContext);
 
         ConsumerNetworkClient consumerNetworkClient = (ConsumerNetworkClient) ReflectionUtils.getFieldValue(aopContext.getTarget(), "client");
+        if (!setContextOnNetworkClient(kafkaPluginContext, consumerNetworkClient)) {
+            // Check if the KafkaConsumer is the type of higher version.
+            // The Higher version of the Kafka client wraps the consumer into a 'delegate' property
+            Object consumerDelegate = ReflectionUtils.getFieldValue(aopContext.getTarget(), "delegate");
+            String clazzName = consumerDelegate.getClass().getSimpleName();
+            if ("LegacyKafkaConsumer".equals(clazzName)) {
+                if (consumerDelegate instanceof IBithonObject) {
+                    // The LegacyKafkaConsumer is also instrumented
+                    ((IBithonObject) consumerDelegate).setInjectedObject(kafkaPluginContext);
+                }
+
+                setContextOnNetworkClient(kafkaPluginContext, ReflectionUtils.getFieldValue(consumerDelegate, "client"));
+            } else if ("AsyncKafkaConsumer".equals(clazzName)) {
+                // TODO:
+            }
+        }
+    }
+
+    /**
+     * Set context to the {@link org.apache.kafka.clients.NetworkClient} object inside the {@link ConsumerNetworkClient}
+     *
+     * @param consumerNetworkClient type of {@link ConsumerNetworkClient}
+     */
+    private boolean setContextOnNetworkClient(KafkaPluginContext ctx, Object consumerNetworkClient) {
         Object kafkaNetworkClient = ReflectionUtils.getFieldValue(consumerNetworkClient, "client");
         if (kafkaNetworkClient instanceof IBithonObject) {
-            ((IBithonObject) kafkaNetworkClient).setInjectedObject(kafkaPluginContext);
+            ((IBithonObject) kafkaNetworkClient).setInjectedObject(ctx);
+            return true;
+        } else {
+            return false;
         }
     }
 }
