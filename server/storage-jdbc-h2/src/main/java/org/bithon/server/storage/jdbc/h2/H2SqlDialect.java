01f22b70317588b94942618d0f9c8a13441ace41
frank chen <frank.chen021@outlook.com>
2024-11-27 19:40:10 +0800

    Apply sql dialect's transformation to count/select/dimension queries

--- server/storage-jdbc-h2/src/main/java/org/bithon/server/storage/jdbc/h2/H2SqlDialect.java
+++ server/storage-jdbc-h2/src/main/java/org/bithon/server/storage/jdbc/h2/H2SqlDialect.java
@@ -97,7 +97,7 @@ public class H2SqlDialect implements ISqlDialect {
 
     @Override
     public IExpression transform(IExpression expression) {
-        return expression.accept(new ExpressionOptimizer.AbstractOptimizer() {
+        return expression == null ? null : expression.accept(new ExpressionOptimizer.AbstractOptimizer() {
             /**
              * H2 does not support Map, the JSON formatted string is stored in the column.
              * So we turn the MapAccessExpression into a LIKE expression
