5eddf883745e85ee5c0ff8376b21ab1dd3485b10
Frank Chen <frank.chen021@outlook.com>
2025-01-02 23:35:07 +0800

    Add clickhouse jdbc

---
+++ agent/agent-plugins/jdbc/clickhouse/src/main/java/org/bithon/agent/plugin/jdbc/clickhouse/ClickHousePlugin.java
@@ -0,0 +1,73 @@
+/*
+ *    Copyright 2020 bithon.org
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.bithon.agent.plugin.jdbc.clickhouse;
+
+import org.bithon.agent.instrumentation.aop.interceptor.descriptor.InterceptorDescriptor;
+import org.bithon.agent.instrumentation.aop.interceptor.matcher.Matchers;
+import org.bithon.agent.instrumentation.aop.interceptor.plugin.IPlugin;
+import org.bithon.shaded.net.bytebuddy.description.modifier.Visibility;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.bithon.agent.instrumentation.aop.interceptor.descriptor.InterceptorDescriptorBuilder.forClass;
+
+/**
+ * @author frankchen
+ */
+public class ClickHousePlugin implements IPlugin {
+
+    @Override
+    public List<InterceptorDescriptor> getInterceptors() {
+
+        return Arrays.asList(
+
+            // JdbcConnection
+            forClass("org.h2.jdbc.JdbcConnection")
+                .onConstructor()
+                .interceptedBy("org.bithon.agent.plugin.jdbc.h2.JdbcConnection$Ctor")
+                .build(),
+
+            // PreparedStatement
+            forClass("org.h2.jdbc.JdbcPreparedStatement")
+                .onConstructor()
+                .andArgs(1, "java.lang.String")
+                .interceptedBy("org.bithon.agent.plugin.jdbc.h2.JdbcPreparedStatement$Ctor")
+
+                .onMethod(Matchers.names("execute", "executeQuery", "executeUpdate"))
+                .andVisibility(Visibility.PUBLIC)
+                .andNoArgs()
+                .interceptedBy("org.bithon.agent.plugin.jdbc.h2.JdbcPreparedStatement$Execute")
+
+                .build(),
+
+            // Statement
+            forClass("org.h2.jdbc.JdbcStatement")
+                .onMethod(Matchers.names("execute", "executeQuery", "executeUpdate", "executeLargeUpdate"))
+                .andVisibility(Visibility.PUBLIC)
+                .andArgs(0, "java.lang.String")
+                .interceptedBy("org.bithon.agent.plugin.jdbc.h2.JdbcStatement$Execute")
+
+                .onMethod(Matchers.names("executeBatch", "executeLargeBatch"))
+                .andVisibility(Visibility.PUBLIC)
+                .andNoArgs()
+                .interceptedBy("org.bithon.agent.plugin.jdbc.h2.JdbcStatement$ExecuteBatch")
+
+                .build()
+        );
+    }
+}
