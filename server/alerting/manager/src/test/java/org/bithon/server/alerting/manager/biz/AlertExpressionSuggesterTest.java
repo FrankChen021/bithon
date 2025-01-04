7ce17e74ea478e2181e92fcf902220eff6db6be7
Frank Chen <frank.chen021@outlook.com>
2025-01-04 23:38:27 +0800

    Fix style

--- server/alerting/manager/src/test/java/org/bithon/server/alerting/manager/biz/AlertExpressionSuggesterTest.java
+++ server/alerting/manager/src/test/java/org/bithon/server/alerting/manager/biz/AlertExpressionSuggesterTest.java
@@ -273,12 +273,12 @@ public class AlertExpressionSuggesterTest {
         // all 3 columns are suggested
         Assert.assertEquals(3, suggestionList.size());
         Assert.assertEquals("appName", suggestionList.get(0).getText());
-        Assert.assertEquals("Dimension", ((AlertExpressionSuggester.SuggestionTag)suggestionList.get(0).getTag()).getTagText());
+        Assert.assertEquals("Dimension", ((AlertExpressionSuggester.SuggestionTag) suggestionList.get(0).getTag()).getTagText());
 
         Assert.assertEquals("eventCount", suggestionList.get(1).getText());
-        Assert.assertEquals("Metric", ((AlertExpressionSuggester.SuggestionTag)suggestionList.get(1).getTag()).getTagText());
+        Assert.assertEquals("Metric", ((AlertExpressionSuggester.SuggestionTag) suggestionList.get(1).getTag()).getTagText());
 
         Assert.assertEquals("instanceName", suggestionList.get(2).getText());
-        Assert.assertEquals("Dimension", ((AlertExpressionSuggester.SuggestionTag)suggestionList.get(2).getTag()).getTagText());
+        Assert.assertEquals("Dimension", ((AlertExpressionSuggester.SuggestionTag) suggestionList.get(2).getTag()).getTagText());
     }
 }
