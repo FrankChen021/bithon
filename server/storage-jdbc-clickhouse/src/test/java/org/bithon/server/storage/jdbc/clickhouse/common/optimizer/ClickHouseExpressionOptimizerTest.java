b033f064107b5072d39ea74a4720f2198a77905a
Frank Chen <frank.chen021@outlook.com>
2025-04-14 21:11:33 +0800

    Fix out of range exception (#1003)

--- server/storage-jdbc-clickhouse/src/test/java/org/bithon/server/storage/jdbc/clickhouse/common/optimizer/ClickHouseExpressionOptimizerTest.java
+++ server/storage-jdbc-clickhouse/src/test/java/org/bithon/server/storage/jdbc/clickhouse/common/optimizer/ClickHouseExpressionOptimizerTest.java
@@ -20,8 +20,16 @@ import org.bithon.component.commons.expression.IExpression;
 import org.bithon.component.commons.expression.function.Functions;
 import org.bithon.component.commons.expression.serialization.IdentifierQuotaStrategy;
 import org.bithon.server.storage.common.expression.ExpressionASTBuilder;
+import org.bithon.server.storage.datasource.DefaultSchema;
+import org.bithon.server.storage.datasource.ISchema;
+import org.bithon.server.storage.datasource.TimestampSpec;
+import org.bithon.server.storage.datasource.column.StringColumn;
+import org.bithon.server.storage.datasource.column.aggregatable.sum.AggregateLongSumColumn;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 
 /**
@@ -89,6 +97,24 @@ public class ClickHouseExpressionOptimizerTest {
 
         Assertions.assertEquals(
             "hasToken(a, 'SERVER') AND hasToken(a, 'ERROR') AND (a like '%SERVER\\_ERROR%') AND hasToken(a, 'EXCEPTION') AND hasToken(a, 'CODE') AND (a like '%EXCEPTION\\_CODE%')",
+            expr.serializeToText(IdentifierQuotaStrategy.NONE));
+    }
+
+    @Test
+    public void test_ZeroCountAggregateFunction_OptimizationDoNothing() {
+        ISchema schema = new DefaultSchema("test-metrics",
+                                           "test-metrics",
+                                           new TimestampSpec("timestamp"),
+                                           Arrays.asList(new StringColumn("appName", "appName"), new StringColumn("type", "type")),
+                                           Collections.singletonList(new AggregateLongSumColumn("metric", "metric")));
+
+        IExpression expr = ExpressionASTBuilder.builder()
+                                               .functions(Functions.getInstance())
+                                               .build("count()")
+                                               .accept(new ClickHouseExpressionOptimizer(schema));
+
+        Assertions.assertEquals(
+            "count()",
             expr.serializeToText(IdentifierQuotaStrategy.NONE));
     }
 }
