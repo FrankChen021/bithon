b4dee22acde11ab31f0ae90e426313776559d35b
Frank Chen <frank.chen021@outlook.com>
2025-05-17 11:33:10 +0800

    Revert

--- server/pipeline/src/main/java/org/bithon/server/pipeline/tracing/sampler/TraceSamplingEnabler.java
+++ server/pipeline/src/main/java/org/bithon/server/pipeline/tracing/sampler/TraceSamplingEnabler.java
@@ -31,29 +31,19 @@ import org.springframework.core.type.AnnotatedTypeMetadata;
 public class TraceSamplingEnabler implements Condition {
 
     /**
-     * This condition will be evaluated for multiple times(have no time to find why)
-     * So we have to cache the result
-     */
-    private static Boolean enabled = null;
-
-    /**
      * Use {@link TracePipelineConfig} to check because it defines default value for some properties
      */
     @Override
     public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-        if (enabled == null) {
-            ConfigurationProperties properties = TracePipelineConfig.class.getAnnotation(ConfigurationProperties.class);
-            String propertyPath = properties.prefix();
+        ConfigurationProperties properties = TracePipelineConfig.class.getAnnotation(ConfigurationProperties.class);
+        String propertyPath = properties.prefix();
 
-            // Since TraceSamplingEnable is initialized before EnvironmentBinder is injected, we have to construct a new EnvironmentBinder
-            TracePipelineConfig config = EnvironmentBinder.from((ConfigurableEnvironment) context.getEnvironment())
-                                                          .bind(propertyPath, TracePipelineConfig.class);
+        // Since TraceSamplingEnable is initialized before EnvironmentBinder is injected, we have to construct a new EnvironmentBinder
+        TracePipelineConfig config = EnvironmentBinder.from((ConfigurableEnvironment) context.getEnvironment())
+                                                      .bind(propertyPath, TracePipelineConfig.class);
 
-            enabled = config != null
-                      && config.isEnabled()
-                      && config.isMetricOverSpanEnabled();
-        }
-
-        return enabled;
+        return config != null
+               && config.isEnabled()
+               && config.isMetricOverSpanEnabled();
     }
 }
