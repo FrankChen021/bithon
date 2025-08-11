4360c886f0df6bced4ea41b4f071428e86868182
Frank Chen <frank.chen021@outlook.com>
2025-08-12 00:29:48 +0800

    solve race condition

--- agent/agent-plugins/httpclient/reactor/src/main/java/org/bithon/agent/plugin/httpclient/reactor/interceptor/HttpClientConfig$DefaultConnectionObserver.java
+++ agent/agent-plugins/httpclient/reactor/src/main/java/org/bithon/agent/plugin/httpclient/reactor/interceptor/HttpClientConfig$DefaultConnectionObserver.java
@@ -25,6 +25,8 @@ import reactor.netty.Connection;
 import reactor.netty.ConnectionObserver;
 import reactor.netty.http.client.HttpClientConfig;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * {@link reactor.netty.http.client.HttpClientConfig#defaultConnectionObserver()}
  *
@@ -54,6 +56,7 @@ public class HttpClientConfig$DefaultConnectionObserver extends AfterInterceptor
     private static class ConnectionStateObserver implements ConnectionObserver {
         private ITraceSpan span;
         private final String connectionProvider;
+        private final ReentrantLock lock = new ReentrantLock();
 
         public ConnectionStateObserver(HttpClientConfig clientConfig, ITraceSpan span) {
             this.span = span;
@@ -62,29 +65,42 @@ public class HttpClientConfig$DefaultConnectionObserver extends AfterInterceptor
 
         @Override
         public void onUncaughtException(Connection connection, Throwable error) {
-            span.tag(error).finish();
-            span.context().finish();
+            lock.lock();
+            try {
+                if (span != null) {
+                    span.tag(error).finish();
+                    span.context().finish();
+                    span = null;
+                }
+            } finally {
+                lock.unlock();
+            }
         }
 
         @Override
         public void onStateChange(Connection connection, State newState) {
-            if (newState.equals(State.CONNECTED) && span != null) {
-                span.tag(Tags.Net.PEER, connection.channel().remoteAddress())
-                    .tag(Tags.Http.CLIENT, "reactor")
-                    // Since there is a network connection to remote, we mark it as a client span
-                    .kind(SpanKind.CLIENT)
-                    .finish();
-                span.context().finish();
-                span = null;
-            }
-            if (newState.equals(State.ACQUIRED) && span != null) {
-                // A connection is retrieved from the pool,
-                // set the span name to 'acquire' and do not mark it as SpanKind.CLIENT
-                span.method(connectionProvider, "acquire")
-                    .tag(Tags.Net.PEER, connection.channel().remoteAddress())
-                    .finish();
-                span.context().finish();
-                span = null;
+            lock.lock();
+            try {
+                if (newState.equals(State.CONNECTED) && span != null) {
+                    span.tag(Tags.Net.PEER, connection.channel().remoteAddress())
+                        .tag(Tags.Http.CLIENT, "reactor")
+                        // Since there is a network connection to remote, we mark it as a client span
+                        .kind(SpanKind.CLIENT)
+                        .finish();
+                    span.context().finish();
+                    span = null;
+                }
+                if (newState.equals(State.ACQUIRED) && span != null) {
+                    // A connection is retrieved from the pool,
+                    // set the span name to 'acquire' and do not mark it as SpanKind.CLIENT
+                    span.method(connectionProvider, "acquire")
+                        .tag(Tags.Net.PEER, connection.channel().remoteAddress())
+                        .finish();
+                    span.context().finish();
+                    span = null;
+                }
+            } finally {
+                lock.unlock();
             }
         }
     }
