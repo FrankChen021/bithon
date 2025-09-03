4fc67132a36c56b999ab3692e2bce7f3fe0047bd
Frank Chen <frank.chen021@outlook.com>
2025-09-03 22:51:19 +0800

    rename

---
+++ agent/agent-plugins-test/src/test/java/org/bithon/agent/plugins/test/kafka/KafkaPluginInterceptorTest.java
@@ -0,0 +1,54 @@
+/*
+ *    Copyright 2020 bithon.org
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.bithon.agent.plugins.test.kafka;
+
+import org.bithon.agent.instrumentation.aop.interceptor.plugin.IPlugin;
+import org.bithon.agent.plugin.apache.kafka.KafkaPlugin;
+import org.bithon.agent.plugins.test.AbstractPluginInterceptorTest;
+import org.bithon.agent.plugins.test.MavenArtifact;
+import org.bithon.agent.plugins.test.MavenArtifactClassLoader;
+
+/**
+ * Test case for Apache Kafka plugin (base version)
+ * 
+ * @author frankchen
+ */
+public class KafkaPluginInterceptorTest extends AbstractPluginInterceptorTest {
+    @Override
+    protected IPlugin getPlugin() {
+        return new KafkaPlugin();
+    }
+
+    @Override
+    protected ClassLoader getCustomClassLoader() {
+        return MavenArtifactClassLoader.create(
+            MavenArtifact.of("org.apache.kafka",
+                             "kafka-clients",
+                             "1.0.0"),
+
+            MavenArtifact.of("org.springframework",
+                                "spring-context",
+                                "5.2.15.RELEASE"),
+            MavenArtifact.of("org.springframework",
+                             "spring-tx",
+                             "5.2.15.RELEASE"),
+            MavenArtifact.of("org.springframework.kafka",
+                             "spring-kafka",
+                             "2.2.7.RELEASE")
+        );
+    }
+}
---
+++ agent/agent-plugins-test/src/test/java/org/bithon/agent/plugins/test/spring/SpringWebMvcPluginInterceptorTest.java
@@ -0,0 +1,50 @@
+/*
+ *    Copyright 2020 bithon.org
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.bithon.agent.plugins.test.spring;
+
+import org.bithon.agent.instrumentation.aop.interceptor.plugin.IPlugin;
+import org.bithon.agent.plugin.spring.webmvc.SpringWebMvcPlugin;
+import org.bithon.agent.plugins.test.AbstractPluginInterceptorTest;
+import org.bithon.agent.plugins.test.MavenArtifact;
+import org.bithon.agent.plugins.test.MavenArtifactClassLoader;
+
+/**
+ * Test case for Spring WebMVC plugin
+ *
+ * @author frankchen
+ */
+public class SpringWebMvcPluginInterceptorTest extends AbstractPluginInterceptorTest {
+    @Override
+    protected IPlugin getPlugin() {
+        return new SpringWebMvcPlugin();
+    }
+
+    @Override
+    protected ClassLoader getCustomClassLoader() {
+        return MavenArtifactClassLoader.create(
+            MavenArtifact.of("org.springframework",
+                             "spring-webmvc",
+                             "4.3.12.RELEASE"),
+            MavenArtifact.of("org.springframework",
+                             "spring-core",
+                             "4.3.12.RELEASE"),
+            MavenArtifact.of("org.springframework",
+                             "spring-web",
+                             "4.3.12.RELEASE")
+        );
+    }
+}
